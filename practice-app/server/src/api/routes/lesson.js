bfa06dc608466caed76879b8adfdaa9049453fc7
Egemen Atik <egemenatikk@gmail.com>
2022-05-16 14:06:36 +0300

    Implemented get lesson events method

--- practice-app/server/src/api/routes/lesson.js
+++ practice-app/server/src/api/routes/lesson.js
@@ -1,8 +1,10 @@
 import { Router } from 'express';
 import { getLessonsByCategory } from '../controllers/lesson/index.js';
+import { getLessonEvents } from '../controllers/lesson/index.js';
 
 const router = Router();
 
 router.get('/byCategory', getLessonsByCategory);
+router.get('/events/:lesson_id', getLessonEvents);
 
 export default router
