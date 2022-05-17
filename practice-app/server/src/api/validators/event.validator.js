438f3f8fc68f987be5b9fa953894f57906510702
xltvy <68481602+xltvy@users.noreply.github.com>
2022-05-17 20:13:02 +0300

    Added host_id and lesson_id properties for event schema

--- practice-app/server/src/api/validators/event.validator.js
+++ practice-app/server/src/api/validators/event.validator.js
@@ -4,7 +4,9 @@ export function validateEvent(body) {
   const schema = Joi.object({
     title: Joi.string().min(3).required(),
     date: Joi.date().required(),
-    location: Joi.string().min(3).required()
+    location: Joi.string().min(3).required(),
+    host_id: Joi.string().hex().length(24).required(),
+    lesson_id: Joi.string().hex().length(24).required()
   });
   return schema.validate(body);
 }
--- practice-app/server/src/models/event.js
+++ practice-app/server/src/models/event.js
@@ -16,7 +16,9 @@ const eventSchema = new Schema({
     location: {
         type: String,
         required: true,
-    }
+    },
+    host_id: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
+    lesson_id: { type: mongoose.Schema.Types.ObjectId, ref: 'Lesson', required: true }
 });
 
 const Event = model('Event', eventSchema);
