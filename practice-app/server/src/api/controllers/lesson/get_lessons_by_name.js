8fa67f5d3d5609202246def6653be22ae632fb7f
onurkmr <konur2727@gmail.com>
2022-05-17 22:23:00 +0300

    Empty lesson array is catched

--- practice-app/server/src/api/controllers/lesson/get_lessons_by_name.js
+++ practice-app/server/src/api/controllers/lesson/get_lessons_by_name.js
@@ -1,21 +1,21 @@
-import mongoose from 'mongoose';
 import { Lesson } from '../../../models/index.js';
-import { Category } from '../../../models/index.js';
 
 export default async (req, res) => {
   const name = req.query.name;
   
-  if (!name || typeof name === 'string' || name instanceof String) {
-    return res.status(400).json({ "resultMessage": name });
-  }
-
+  if (!name) 
+    return res.status(400).json({ "resultMessage": "Please provide a valid name." });
+  
   const lessons = await Lesson.find({ name: name })
     .catch((err) => {
-      return res.status(500).json({ "resultMessage": "Wrong name." });
-    });
-
-  return res.status(200).json({
-    resultMessage: "Lessons are successfully fetched.",
-    lessons: lessons,
+    return res.status(500).json({ "resultMessage": err.message });
   });
+
+  if (lessons.length != 0)
+    return res.status(200).json({
+      resultMessage: "Lessons are successfully fetched.",
+      lessons: lessons,
+    });
+  
+  return res.status(404).json({ "resultMessage": "Lesson cannot found."});
 };
