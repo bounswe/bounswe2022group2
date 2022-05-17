f18dacb7bd99d9924ec78cff69907ef71121a443
onurkmr <konur2727@gmail.com>
2022-05-17 20:44:24 +0300

    Getting lesson by name endpoint is added.

--- practice-app/server/src/api/controllers/lesson/index.js
+++ practice-app/server/src/api/controllers/lesson/index.js
@@ -1,1 +1,2 @@
 export { default as getLessonsByCategory } from './get_lessons_by_category.js';
+export { default as getLessonsByName } from './get_lessons_by_name.js';
--- practice-app/server/src/api/routes/lesson.js
+++ practice-app/server/src/api/routes/lesson.js
@@ -1,8 +1,10 @@
 import { Router } from 'express';
 import { getLessonsByCategory } from '../controllers/lesson/index.js';
+import { getLessonsByName } from '../controllers/lesson/index.js';
 
 const router = Router();
 
 router.get('/byCategory', getLessonsByCategory);
+router.get('/byName', getLessonsByName);
 
 export default router
