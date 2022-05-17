00d426bd06237d79f8df3ba018e8a8a47112cc45
onurkmr <konur2727@gmail.com>
2022-05-17 22:32:41 +0300

    Password issue is solved.

--- practice-app/server/src/api/controllers/user/login.js
+++ practice-app/server/src/api/controllers/user/login.js
@@ -1,7 +1,6 @@
 import { User } from '../../../models/index.js';
 import { validateLogin } from '../../validators/user.validator.js';
 import bcrypt from 'bcryptjs';
-const { hash } = bcrypt;
 
 export default async (req,res) => {
     const { error } = validateLogin(req.body);
@@ -18,30 +17,18 @@ export default async (req,res) => {
     if (!emailCheck) 
         return res.status(404).json({"resultMessage": "There is no user with the given email."});
 
-    /*
-    let passwordCheck = await User.exists({ email: email, password: incomingPassword})
+    let user1 = await User.find({ email: email }).select('+password')
         .catch((err) => {
         return res.status(500).json({ "resultMessage": err.message });
     });
-
-    if (!passwordCheck) 
-        return res.status(404).json({"resultMessage": "Wrong password."});
-    */
-    let user1 = await User.find({ email: email })
-        .catch((err) => {
-        return res.status(500).json({ "resultMessage": err.message });
-    });
-    console.log(user);
-    console.log(user.password);
     let user = user1[0];
     const incomingPassword = req.body.password;
     const databasePassword = user.password;
-
     const passwordCheck = bcrypt.compareSync(incomingPassword, databasePassword);
 
     if (!passwordCheck) 
         return res.status(404).json({"resultMessage": "Wrong password."});
-    
+
     return res.status(200).json({
         resultMessage: "Successfully logged in.",
         user: user.toJSON(),
