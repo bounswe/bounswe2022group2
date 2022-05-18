2f757fd271139c18b52f18ed83fcaac2015fe91b
surmelienes1 <74325266+surmelienes1@users.noreply.github.com>
2022-05-18 14:43:41 +0300

    Create getEventDetails.js

---
+++ practice-app/server/src/api/controllers/event/getEventDetails.js
@@ -0,0 +1,26 @@
+import { User } from '../../../models/index.js';
+import { Event } from '../../../models/index.js';
+import { validateEvent } from '../../validators/event.validator.js';
+
+export default async (req, res) => {
+
+  const { error } = validateEvent(req.body);
+
+  if (error) {
+    return res.status(400).json({ "resultMessage": error.details[0].message });
+  }
+
+  const eventId = req.body.event_id;
+
+  const event = await Event.findById(eventId)
+  .catch(err => {
+    return res.status(500).json({ "resultMessage": err.message });
+  });
+
+  if(!event) return res.status(404).json({"resultMessage": "The event with the given id could not be found"});
+
+  return res.status(200).json({
+    resultMessage: "Event is successfully fetched.", event: event
+  });
+
+}
