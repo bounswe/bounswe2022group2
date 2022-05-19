eb05a8e0a4230ac2032d0f537f0d9e6a4e81da8a
Altay Acar <68481602+xltvy@users.noreply.github.com>
2022-05-19 19:07:40 +0300

    Delete event.js
    Mistakenly committed file

--- practice-app/server/src/api/routes/event.js
+++
@@ -1,8 +0,0 @@
-import { Router } from 'express';
-import { createEventEndpoint } from '../controllers/event/index.js';
-
-const router = Router();
-
-router.post('/createEvent', createEventEndpoint);
-
-export default router
