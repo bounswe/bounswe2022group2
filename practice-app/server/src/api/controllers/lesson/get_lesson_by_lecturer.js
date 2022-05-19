58ff0c29168f8ff631df2ef282cde897176a25de
ezgy <ezgibati@gmail.com>
2022-05-19 21:13:20 +0300

    added suggested check for user validity

--- practice-app/server/src/api/controllers/lesson/get_lesson_by_lecturer.js
+++ practice-app/server/src/api/controllers/lesson/get_lesson_by_lecturer.js
@@ -1,11 +1,17 @@
 import mongoose from 'mongoose';
 import { Lesson } from '../../../models/index.js';
+import { User } from '../../../models/index.js';
 
 export default async (req, res) => {
   const lecturerId = req.query.lecturer;
   if (!lecturerId || !mongoose.isValidObjectId(lecturerId)) {
     return res.status(400).json({ "resultMessage": "Please provide a valid lecturer id." });
   }
+
+  const lecturer = await User.findOne({ _id: lecturerId})
+  .catch((err) => {
+      return res.status(404).json({ "resultMessage": err.message });;
+  });
 
   const lessons = await Lesson.find({ lecturer: lecturerId })
     .catch((err) => {
