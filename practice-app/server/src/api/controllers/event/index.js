fbcac8fb0bc1649038a1d88e75312e959e3f8e78
surmelienes1 <74325266+surmelienes1@users.noreply.github.com>
2022-05-19 15:06:51 +0300

    Update index.js

--- practice-app/server/src/api/controllers/event/index.js
+++ practice-app/server/src/api/controllers/event/index.js
@@ -1,1 +1,3 @@
+export { default as createEventEndpoint } from './createEventEndpoint.js';
+export { createEvent } from './createEvent.js';
 export { default as getEventDetails } from './getEventDetails.js';
