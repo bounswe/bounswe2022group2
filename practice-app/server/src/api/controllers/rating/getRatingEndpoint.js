57540ee50b5694c3da11084943565033393c1ed7
codingAku <egoldferb@gmail.com>
2022-05-19 17:28:26 +0300

    fixing the implementation of unit tests for get and post rating endpoints

--- practice-app/server/src/api/controllers/rating/getRatingEndpoint.js
+++ practice-app/server/src/api/controllers/rating/getRatingEndpoint.js
@@ -1,16 +1,24 @@
 import { Rating } from '../../../models/index.js';
+import Lesson from '../../../models/lesson.js';
 import { validateGetReqBody } from '../../validators/rating.validator.js';
 
 export default async (req, res) => {
     let status = 200;
-    const { error } = validateGetReqBody(req.body);
+    const { error } = validateGetReqBody(req.query);
     if(error) {
         status = 400;
         return res.status(status).json({ "resultMessage": error.message });
     }
-    const ratings = await Rating.find({rating: {$gte: req.body.min, $lte: req.body.max}}).catch(err=>{
+    const ratings = await Rating.find({rating: {$gte: req.query.min, $lte: req.query.max}})
+    .catch(err=>{
         return res.status(500).json({ "resultMessage": err.message });
     });
+    ratings.forEach(async element => {
+ 
+        const lesson = await Lesson.findById(element.lessonID.toString());
+        element.name = lesson.name;
+    });
     return res.status(200).json(ratings);
-    
+
 };
+
