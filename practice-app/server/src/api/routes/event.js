6f2d46a852efdd88d74316322b8220b95ee6965d
surmelienes1 <74325266+surmelienes1@users.noreply.github.com>
2022-05-19 15:08:12 +0300

    Update event.js

--- practice-app/server/src/api/routes/event.js
+++ practice-app/server/src/api/routes/event.js
@@ -1,8 +1,10 @@
 import { Router } from 'express';
+import { createEventEndpoint } from '../controllers/event/index.js';
 import { getEventDetails } from '../controllers/event/index.js';
 
-
 const router = Router();
+
+router.post('/createEvent', createEventEndpoint);
 
 router.get("/details", getEventDetails);
 
