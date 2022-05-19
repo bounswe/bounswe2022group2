66c095d00028f9cd4a93b73e62d025d555e74eb7
Hasan Can Erol <hasan.erol@boun.edu.tr>
2022-05-20 00:04:04 +0300

    Create enroll lesson unit test
    As mentioned in the [issue 250](https://github.com/bounswe/bounswe2022group2/issues/250) I will be implementing the unit tests for POST endpoint of enrolling lessons.
    
    1. Successful enrolling to a lesson
    2. Request with missing user_id
    3. Request with missing lesson_id
    4. Request with invalid user_id
    5. Request with invalid lesson_id
    6. Request with user_id which does not exist
    7. Request with lesson_id which does not exist
    8. Request with user_id which is already taken this course

---
+++ practice-app/server/test/enroll_lesson.test.js
@@ -0,0 +1,209 @@
+import { expect } from 'expect';
+import mongoose from 'mongoose';
+import request from 'supertest';
+import { Category, User, Lesson } from '../src/models/index.js';
+import app from './../src/app.js';
+
+const categoryId = mongoose.Types.ObjectId();
+const lesson_ids = [mongoose.Types.ObjectId(), mongoose.Types.ObjectId(), mongoose.Types.ObjectId(), mongoose.Types.ObjectId()];
+const user_ids = [mongoose.Types.ObjectId(), mongoose.Types.ObjectId()];
+const invalid_ids = [mongoose.Types.ObjectId(), mongoose.Types.ObjectId()];
+
+const category = {
+  _id: categoryId,
+  title: "Category1",
+  description: "Description1",
+}
+
+const lessons = [
+  {
+    _id: lesson_ids[0],
+    name: "Lesson1",
+    category_id: category._id
+  },
+  {
+    _id: lesson_ids[1],
+    name: "Lesson2",
+    category_id: category._id
+  },
+  {
+    _id: lesson_ids[2],
+    name: "Lesson3",
+    category_id: category._id
+  }
+];
+
+const users = [
+    {
+        _id: user_ids[0],
+        email: "user1@gmail.com",
+        password: "password1",
+        name: "username1",
+        enrolledLessons: [lesson_ids[0],lesson_ids[1],lesson_ids[2]]
+    },
+    {
+        _id: user_ids[1],
+        email: "user2@gmail.com",
+        password: "password2",
+        name: "username2",
+        enrolledLessons: [lesson_ids[1],lesson_ids[2]]
+    }
+]
+
+const setCategory = async () => {
+  await new Category(category).save();
+}
+
+const setLessons = async () => {
+  await new Lesson(lessons[0]).save();
+  await new Lesson(lessons[1]).save();
+  await new Lesson(lessons[2]).save();
+}
+
+const setUsers = async () => {
+    await new User(users[0]).save();
+    await new User(users[1]).save();
+}
+
+const addDummyData = async () => {
+    await setCategory();
+    await setLessons();
+    await setUsers();
+  };
+
+  describe('POST /user/enroll', () => {
+    const enrollUrl = '/user/enroll';
+    beforeEach(addDummyData);
+    
+    it('should return validation error if the user_id is missing in body', (done) => {
+      request(app)
+        .post(enrollUrl)
+        .send({lesson_id: lesson_ids[0]})
+        .expect(400)
+        .end(done);
+    });
+  
+    it('should return validation error if the lesson_id is missing in body', (done) => {
+      request(app)
+        .post(enrollUrl)
+        .send({user_id: user_ids[0]})
+        .expect(400)
+        .end(done);
+    });
+  
+    it('should return validation error if the user_id is invalid in body', (done) => {
+      request(app)
+        .post(enrollUrl)
+        .send({
+          user_id: "id",  
+          lesson_id: lesson_ids[0]
+         })
+        .expect(400)
+        .end(done);
+    });
+  
+    it('should return validation error if the lesson_id is invalid in body', (done) => {
+      request(app)
+        .post(enrollUrl)
+        .send({
+          user_id: user_ids[0],  
+          lesson_id: "55444645161"
+         })
+        .expect(400)
+        .end(done);
+    });
+  
+    it('should return not found error if the user with the given user_id does not exist', (done) => {
+      request(app)
+        .post(enrollUrl)
+        .send({
+          user_id: invalid_ids[0],  
+          lesson_id: lesson_ids[0]
+         })
+        .expect(404)
+        .end(done);
+    });
+  
+    it('should return not found error if the lesson with the given lesson_id does not exist', (done) => {
+      request(app)
+        .post(enrollUrl)
+        .send({
+          user_id: user_ids[0],  
+          lesson_id: invalid_ids[1]
+         })
+        .expect(404)
+        .end(done);
+    });
+  
+    it('should return not found error if the user with the given user_id does take the lesson with the given lesson_id', (done) => {
+      request(app)
+        .post(enrollUrl)
+        .send({
+          user_id: user_ids[0],  
+          lesson_id: lesson_ids[0]
+         })
+        .expect(404)
+        .end(done);
+    });
+  
+    it('should return the user with the given user_id with updated enrolledLessons list and added lesson with the given lesson_id', (done) => {
+  
+      request(app)
+        .post(enrollUrl)
+        .send({ user_id: user_ids[1], lesson_id: lesson_ids[0] })
+        .expect((res) => {
+          expect(res.body.user).not.toBeNull();
+          expect(res.body.user).not.toBeUndefined();
+          expect(res.body.user.email).not.toBeNull();
+          expect(res.body.user.email).not.toBeUndefined();
+          expect(res.body.user.name).not.toBeNull();
+          expect(res.body.user.name).not.toBeUndefined();
+          expect(res.body.user.enrolledLessons).not.toBeNull();
+          expect(res.body.user.enrolledLessons).not.toBeUndefined();
+          expect(res.body.user.enrolledLessons).toHaveLength(2);
+          expect(res.body.lesson).not.toBeNull();
+          expect(res.body.lesson).not.toBeUndefined();
+          expect(res.body.lesson.id).not.toBeNull();
+          expect(res.body.lesson.id).not.toBeUndefined();
+          const firstLesson = res.body.user.enrolledLessons[0];
+          expect(firstLesson).not.toBeNull();
+          expect(firstLesson).not.toBeUndefined();
+          expect(res.statusCode).toBe(200);
+        })
+        .end(done);
+    });
+    
+    it('should return the user with the given user_id with remaining enrolledLessons list and removed lesson with the given lesson_id', (done) => {
+  
+      request(app)
+        .post(enrollUrl)
+        .send({ user_id: user_ids[1], lesson_id: lesson_ids[3] })
+        .expect((res) => {
+          expect(res.body.user).not.toBeNull();
+          expect(res.body.user).not.toBeUndefined();
+          expect(res.body.user.email).not.toBeNull();
+          expect(res.body.user.email).not.toBeUndefined();
+          expect(res.body.user.name).not.toBeNull();
+          expect(res.body.user.name).not.toBeUndefined();
+          expect(res.body.user.enrolledLessons).not.toBeNull();
+          expect(res.body.user.enrolledLessons).not.toBeUndefined();
+          expect(res.body.user.enrolledLessons).toHaveLength(1);
+          expect(res.body.lesson).not.toBeNull();
+          expect(res.body.lesson).not.toBeUndefined();
+          expect(res.body.lesson.id).not.toBeNull();
+          expect(res.body.lesson.id).not.toBeUndefined();
+          const firstLesson = res.body.user.enrolledLessons[0];
+          expect(firstLesson).not.toBeNull();
+          expect(firstLesson).not.toBeUndefined();
+          expect(res.statusCode).toBe(200);
+        })
+        .end(done);
+    });
+    
+  });
+  
+  afterEach(async () => {
+    await Category.findByIdAndDelete(category._id);
+    await Lesson.deleteMany({ _id: { $in: lesson_ids } });
+    await User.deleteMany({ _id: {$in: user_ids}});
+  });
