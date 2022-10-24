c3d326383238c59aa1c3548a755765c0b6753788
xltvy <68481602+xltvy@users.noreply.github.com>
2022-10-24 22:54:18 +0300

    Frontend: Sign up page of the application is customized

--- learnify/web/src/pages/SignUpForm.js
+++ learnify/web/src/pages/SignUpForm.js
@@ -1,10 +1,13 @@
 import React, {useState,setState} from 'react';
+import { NavLink} from 'react-router-dom';
 import './style.css'
 function SignUpForm() {
+    
     const [userName, setUserName] = useState(null);
     const [email, setEmail] = useState(null);
     const [password,setPassword] = useState(null);
     const [confirmPassword,setConfirmPassword] = useState(null);
+    const [isChecked, setIsChecked] = useState(false);
 
     const handleInputChange = (e) => {
         const {id , value} = e.target;
@@ -20,7 +23,9 @@ function SignUpForm() {
         if(id === "confirmPassword"){
             setConfirmPassword(value);
         }
-
+        if(id === "isChecked"){
+            setIsChecked(value);
+        }
     }
 
     const handleSubmit  = () => {
@@ -28,30 +33,64 @@ function SignUpForm() {
     }
 
     return(
-        <div className="form">
-            <div className="form-body">
-                <div className="username">
-                    <label className="form__label" for="userName">User Name </label>
-                    <input className="form__input" type="text" value={userName} onChange = {(e) => handleInputChange(e)} id="userName" placeholder="User Name"/>
+        <div className='pageLayout'>
+            <div className='leftPart' />
+            <div className='rightPart'>
+                <div className='welcome-navigation'>
+                    <NavLink to="/" activeClassName="welcome-navigation-item-active" className="welcome-navigation-item">
+                        Sign Up
+                    </NavLink>
+                    <div className='space-8'/>
+                    <NavLink to="/signin" activeClassName="welcome-navigation-item-active" className="welcome-navigation-item">
+                        Sign In
+                    </NavLink>
                 </div>
-                <div className="email">
-                    <label className="form__label" for="email">Email </label>
-                    <input  type="email" id="email" className="form__input" value={email} onChange = {(e) => handleInputChange(e)} placeholder="Email"/>
+                <div className="form">
+                    <div className="form-body">
+                        <div className="username">
+                            <label className="form__label" for="userName">USERNAME </label>
+                            <div className='space-3'></div>
+                            <div>
+                                <input className="form__input" type="text" value={userName} onChange = {(e) => handleInputChange(e)} id="userName" placeholder="User Name"/>
+                            </div>
+                        </div>
+                        <div className="email">
+                            <label className="form__label" for="email">E-MAIL </label>
+                            <div className='space-3'></div>
+                            <div>
+                                <input  type="email" id="email" className="form__input" value={email} onChange = {(e) => handleInputChange(e)} placeholder="Email"/>
+                            </div>
+                        </div>
+                        <div className="password">
+                            <label className="form__label" for="password">PASSWORD </label>
+                            <div className='space-3'></div>
+                            <div>
+                                <input className="form__input" type="password"  id="password" value={password} onChange = {(e) => handleInputChange(e)} placeholder="Password"/>
+                            </div>
+                        </div>
+                        <div className="confirm-password">
+                            <label className="form__label" for="confirmPassword">CONFIRM PASSWORD </label>
+                            <div className='space-3'></div>
+                            <div>
+                                <input className="form__input" type="password" id="confirmPassword" value={confirmPassword} onChange = {(e) => handleInputChange(e)} placeholder="Confirm Password"/>
+                            </div>
+                        </div>
+                        <div className='signup-checkbox-field'>
+                            <label className='checkboxSignUpLabel'>
+                                <input className='signup-checkbox' type="checkbox" value={isChecked} onChange = {(e) => handleInputChange(e)} />
+                                <span> I have read the {" "}<a href="https://www.nvi.gov.tr/kvkk-aydinlatma-metni" target="_blank" rel="noopener noreferrer" className="registerKVKK">
+                                        clarification on KVKK
+                                    </a> and agree on all the terms and conditions.
+                                </span>
+                            </label>
+                        </div>
+                    </div>
+                    <div class="signup-button">
+                        <button onClick={()=>handleSubmit()} type="submit" class="btn-orange">Register</button>
+                    </div>
                 </div>
-                <div className="password">
-                    <label className="form__label" for="password">Password </label>
-                    <input className="form__input" type="password"  id="password" value={password} onChange = {(e) => handleInputChange(e)} placeholder="Password"/>
-                </div>
-                <div className="confirm-password">
-                    <label className="form__label" for="confirmPassword">Confirm Password </label>
-                    <input className="form__input" type="password" id="confirmPassword" value={confirmPassword} onChange = {(e) => handleInputChange(e)} placeholder="Confirm Password"/>
-                </div>
-            </div>
-            <div class="footer">
-                <button onClick={()=>handleSubmit()} type="submit" class="btn">Register</button>
             </div>
         </div>
-       
     )       
 }
 
