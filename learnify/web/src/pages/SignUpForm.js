dab128683222a0caabcf42206adb2d0ff8b2e9c3
xltvy <68481602+xltvy@users.noreply.github.com>
2022-10-24 23:39:53 +0300

    Frontend: Sign up page illustration and logo added

--- learnify/web/src/pages/SignUpForm.js
+++ learnify/web/src/pages/SignUpForm.js
@@ -1,6 +1,9 @@
 import React, {useState,setState} from 'react';
 import { NavLink} from 'react-router-dom';
 import './style.css'
+import logo from '../images/logo-dblue.png'
+import illustration from '../images/learn-illustration.png'
+
 function SignUpForm() {
     
     const [userName, setUserName] = useState(null);
@@ -34,7 +37,17 @@ function SignUpForm() {
 
     return(
         <div className='pageLayout'>
-            <div className='leftPart' />
+            <div className='leftPart'>
+                <div className='signup-page-logo'>
+                    <img src={logo} alt="Learnify Logo" height={80} />
+                </div>
+                <div className='signup-page-illustration'>
+                    <img src={illustration} alt="Welcome Illustration" height={400} />
+                </div>
+                <div className='illustration-text'>
+                    Learnify. Where <span className='bold-text'>learning</span> is achieved <span className='bold-text'>through collaboration.</span>
+                </div>
+            </div>
             <div className='rightPart'>
                 <div className='welcome-navigation'>
                     <NavLink to="/" activeClassName="welcome-navigation-item-active" className="welcome-navigation-item">
