1b847f3c812c46502c94b9b0966a704110610b1c
koraytekin <ge89zub@mytum.de>
2022-10-25 00:47:09 +0300

    Initial version of sign-in page created(not connected to API, forgot password link is directed to sign-up page).

--- learnify/web/src/pages/SignInForm.js
+++ learnify/web/src/pages/SignInForm.js
@@ -1,5 +1,8 @@
 import React, {useState,setState} from 'react';
+import {NavLink} from 'react-router-dom';
 import './style.css'
+import logo from '../images/logo-dblue.png'
+import illustration from '../images/learn-illustration.png'
 function SignInForm() {
 
     const [email, setEmail] = useState(null);
@@ -16,13 +19,33 @@ function SignInForm() {
     }
 
     const handleSubmit  = () => {
+        console.log("email and password was entered as");
         console.log(email,password);
     }
 
     return(
-        <div className="form">
-            <div className="form-body">
-
+        <div className='pageLayout'>
+            <div className='leftPart'>
+                <div className='signin-page-logo'>
+                    <img src={logo} alt="Learnify Logo" height={80} />
+                </div>
+                <div className='signin-page-illustration'>
+                    <img src={illustration} alt="Welcome Illustration" height={400} />
+                </div>
+                <div className='illustration-text'>
+                    Learnify. Where <span className='bold-text'>learning</span> is achieved <span className='bold-text'>through collaboration.</span>
+                </div>
+            </div>
+            <div className="rightPart">
+            <div className='welcome-navigation'>
+                    <NavLink to="/" activeClassName="welcome-navigation-item-active" className="welcome-navigation-item">
+                        Sign Up
+                    </NavLink>
+                    <div className='space-8'/>
+                    <NavLink to="/signin" activeClassName="welcome-navigation-item-active" className="welcome-navigation-item">
+                        Sign In
+                    </NavLink>
+                </div>
                 <div className="email">
                     <label className="form__label" for="email">Email </label>
                     <input  type="email" id="email" className="form__input" value={email} onChange = {(e) => handleInputChange(e)} placeholder="Email"/>
@@ -31,10 +54,14 @@ function SignInForm() {
                     <label className="form__label" for="password">Password </label>
                     <input className="form__input" type="password"  id="password" value={password} onChange = {(e) => handleInputChange(e)} placeholder="Password"/>
                 </div>
-
-            </div>
-            <div class="footer">
+                <div className="signin-button">
                 <button onClick={()=>handleSubmit()} type="submit" class="btn">Register</button>
+                </div>
+                
+                    <NavLink to="/" className="additional-Nav-Text">
+                    Forgot Password
+                    </NavLink>
+                
             </div>
         </div>
        
