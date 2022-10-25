19a98f0ecce9cf424ed78b2fd49820fb9f833e07
mbatuhancelik <mbatuhancelik>
2022-10-25 14:49:51 +0300

    updating docker so that it takes .env from host

--- learnify/backend/config/index.js
+++ learnify/backend/config/index.js
@@ -2,11 +2,11 @@ import { config } from 'dotenv';
 config();
 
 //NOTE: If you are running the project in an instance, you should store these secret keys in its configuration settings.
-const { PORT, NODE_ENV, DB_URI, MAIL, MAIL_PASS, JWT_KEY } = process.env
+const { PORT, NODE_ENV, DB_URI, MAIL, MAIL_PASS, JWT_KEY , LEARNIFY_DOCKER} = process.env
 
 export const port = PORT || 3000;
 export const env = NODE_ENV;
-export const dbUri = DB_URI;
+export const dbUri = LEARNIFY_DOCKER || DB_URI;
 export const mail = MAIL;
 export const mail_pass = MAIL_PASS;
 export const jwt_key = JWT_KEY;
