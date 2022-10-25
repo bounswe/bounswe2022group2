61bf5c85923a0b028a59f84568157d8e4b421fac
Bahrican Yesil <bahricanyesil@gmail.com>
2022-10-25 09:16:13 +0300

    Removes future to pass the tests

--- learnify/mobile/lib/core/base/view/base_view.dart
+++ learnify/mobile/lib/core/base/view/base_view.dart
@@ -74,10 +74,9 @@ class _BaseViewState<T extends BaseViewModel> extends State<BaseView<T>> {
   @override
   void dispose() {
     if (widget.customDispose != null) widget.customDispose!();
-    model.disposeView();
-    Future<void>.delayed(Duration.zero, () async {
-      await model.customDispose();
-    });
+    model
+      ..disposeView()
+      ..customDispose();
     super.dispose();
   }
 
