aa87f88ebb450516499046a1594799c52194e606
mbatuhancelik <mbatuhancelik>
2022-10-25 14:33:37 +0300

    send verification email

--- learnify/backend/config/index.js
+++ learnify/backend/config/index.js
@@ -2,10 +2,12 @@ import { config } from 'dotenv';
 config();
 
 //NOTE: If you are running the project in an instance, you should store these secret keys in its configuration settings.
-const { PORT, NODE_ENV, DB_URI } = process.env
+const { PORT, NODE_ENV, DB_URI, MAIL, MAIL_PASS, JWT_KEY } = process.env
 
 export const port = PORT || 3000;
 export const env = NODE_ENV;
 export const dbUri = DB_URI;
-
+export const mail = MAIL;
+export const mail_pass = MAIL_PASS;
+export const jwt_key = JWT_KEY;
 export const prefix = '';
