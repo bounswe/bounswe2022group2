82897f18203c07eca2007cb77e5a9e4b1bc4531b
Egemen Atik <egemenatikk@gmail.com>
2022-10-29 23:39:40 +0300

    Add POST request for verifying email

--- learnify/mobile/lib/features/auth/service/l_auth_service.dart
+++ learnify/mobile/lib/features/auth/service/l_auth_service.dart
@@ -3,6 +3,8 @@ import '../../../core/managers/network/models/l_response_model.dart';
 import '../../../core/managers/network/models/message_response.dart';
 import '../forget-password/model/send_verification_request_model.dart';
 import '../signup/model/signup_request_model.dart';
+import '../verification/model/verify_email_request_model.dart';
+import '../verification/model/verify_email_response_model.dart';
 
 /// Abstract base class for auth service, defines the required functions.
 abstract class IAuthService extends BaseService {
@@ -12,4 +14,8 @@ abstract class IAuthService extends BaseService {
   /// Sends verification code to email
   Future<IResponseModel<MessageResponse>> sendVerification(
       SendVerificationRequest body);
+
+  // Sends entered code to check to verify email
+  Future<IResponseModel<VerifyEmailResponse>> verifyEmail(
+      VerifyEmailRequest body);
 }
