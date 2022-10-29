3fcde0054cd316c7f91895abd6d5fb786100f6fc
Egemen Atik <egemenatikk@gmail.com>
2022-10-30 01:01:35 +0300

    Add createdAt and updatedAt fields to User model

--- learnify/mobile/lib/features/auth/verification/model/user_model.dart
+++ learnify/mobile/lib/features/auth/verification/model/user_model.dart
@@ -1,16 +1,27 @@
 import '../../../../core/base/model/base_model.dart';
 
 class User extends BaseModel<User> {
-  const User({this.email, this.isVerified, this.code, this.id});
+  const User({
+    this.email,
+    this.isVerified,
+    this.createdAt,
+    this.updatedAt,
+    this.code,
+    this.id,
+  });
 
   factory User.fromJson(Map<String, dynamic> json) => User(
       email: BaseModel.getByType<String>(json['email']),
       isVerified: BaseModel.getByType<bool>(json['is_verified']),
+      createdAt: BaseModel.getByType<DateTime>(json['createdAt']),
+      updatedAt: BaseModel.getByType<DateTime>(json['updatedAt']),
       code: BaseModel.getByType<String>(json['code']),
       id: BaseModel.getByType<String>(json['id']));
 
   final String? email;
   final bool? isVerified;
+  final DateTime? createdAt;
+  final DateTime? updatedAt;
   final String? code;
   final String? id;
 
@@ -21,10 +32,13 @@ class User extends BaseModel<User> {
   Map<String, dynamic> get toJson => <String, dynamic>{
         'email': email,
         'is_verified': isVerified,
+        'createdAt': createdAt,
+        'updatedAt': updatedAt,
         'code': code,
         'id': id
       };
 
   @override
-  List<Object?> get props => <Object?>[email, isVerified, code, id];
+  List<Object?> get props =>
+      <Object?>[email, isVerified, createdAt, updatedAt, code, id];
 }
