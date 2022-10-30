246f2367726ea9cb3c6dc52ceeea6ebedb03597a
Gokay Yildiz <yildizgokay2@gmail.com>
2022-10-30 11:59:45 +0300

    grammar fix

--- learnify/web/src/pages/EmailVerificationPage.js
+++ learnify/web/src/pages/EmailVerificationPage.js
@@ -16,7 +16,7 @@ function EmailVerificationPage() {
             .required("Verification Code is required!")
             .matches(
                 /^\d{4}$/,
-                "Verification Code must contain exactly 4 digit numbers!"
+                "Verification Code must contain exactly 4 digit number!"
             ),
     })
     const formOptions = { resolver: yupResolver(formSchema) }
