8625f8a59f364f836efefc73382054fe8bd162ea
codingAku <egoldferb@gmail.com>
2022-10-30 13:57:28 +0300

    return 401 with not verified users

--- learnify/backend/api/controllers/auth/login.js
+++ learnify/backend/api/controllers/auth/login.js
@@ -34,6 +34,12 @@ export default async (req,res) => {
 
     if (!passwordCheck) 
         return res.status(409).json({"resultMessage": "Wrong password."});
+    if(!user.is_verified){
+        return res.status(401).json({
+            resultMessage: "User is not verified.",
+            user: user.toJSON(),
+        });
+    }
     console.log(process.env.JWT_KEY)
     const token = jwt.sign(
         { user_id: user._id, email },
@@ -43,6 +49,7 @@ export default async (req,res) => {
         }
         );
 
+    
     return res.status(200).json({
         resultMessage: "Successfully logged in.",
         user: user.toJSON(),
