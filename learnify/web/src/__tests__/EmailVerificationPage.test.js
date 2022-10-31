bee786e6c82bf08d7c4779943545f6a713d040cc
surmelienes1 <74325266+surmelienes1@users.noreply.github.com>
2022-10-31 19:41:16 +0300

    Create EmailVerificationPage.test.js
    Added 3 unit tests for email verification page.

---
+++ learnify/web/src/__tests__/EmailVerificationPage.test.js
@@ -0,0 +1,45 @@
+import React from "react";
+import EmailVerificationPage from './../pages/EmailVerificationPage';
+import { screen, render, fireEvent } from "@testing-library/react";
+import { BrowserRouter } from "react-router-dom";
+
+
+describe.only("EmailVerificationPage", () => {
+
+  test("should render page layout correctly", () => {
+    const {container} = render(<BrowserRouter><EmailVerificationPage /></BrowserRouter>);
+    expect(container.firstChild.classList.contains('pageLayout')).toBe(true);
+  });
+
+  test("should accept input", () => {
+    render(<BrowserRouter><EmailVerificationPage /></BrowserRouter>);
+
+    const verificationCodeInput = screen.getByTestId("verificationCode");
+    const verifyButton = screen.getByTestId("submit");
+    const resendButton = screen.getByTestId("submitResend");
+    expect(verificationCodeInput).toBeInstanceOf(HTMLInputElement);
+    expect(verifyButton).toBeInstanceOf(HTMLButtonElement);
+    expect(resendButton).toBeInstanceOf(HTMLButtonElement);
+
+  });
+
+  test("should render correct error message when invalid verification code is entered", async () => {
+    render(<BrowserRouter><EmailVerificationPage /></BrowserRouter>);
+
+    const verificationCodeInput = screen.getByTestId("verificationCode");
+    const verifyButton = screen.getByTestId("submit");
+    const resendButton = screen.getByTestId("submitResend");
+
+    fireEvent.change(verificationCodeInput, {
+      target: { value: "aa" },
+    });
+
+    fireEvent.submit(verifyButton);
+
+    fireEvent.submit(resendButton);
+
+    await screen.findByText("Verification Code must contain exactly 4 digits!");
+
+  });
+
+});
