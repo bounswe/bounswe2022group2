e10404a475c8aaf20e511d2d6317f8bf5265d58c
surmelienes1 <74325266+surmelienes1@users.noreply.github.com>
2022-11-08 22:15:08 +0300

    Update NotFoundPage.js
    Updated the not found page to adapt different screen resolutions better.

--- learnify/web/src/pages/NotFoundPage.js
+++ learnify/web/src/pages/NotFoundPage.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import {useNavigate} from 'react-router-dom';
 import './style.css'
-import notfound from '../images/404NotFound.png'
+import notfound from '../images/404NotFound.jpg'
 import logo from '../images/logo-dblue.png'
 
 function NotFoundPage() {
@@ -10,15 +10,14 @@ function NotFoundPage() {
 
   return(        
         <div className='notFoundPageLayout'>
-            <div className='welcomeNotFound'>
-                <div className='notfound-page-logo'>
-                    <img src={logo} alt="Learnify Logo" height={60} />
-                </div>
-                <div className='notfoundpage-text'><h1>Oops! Something went wrong!</h1>
-                </div>
+            <div className='notfound-page-logo'>
+                <img  style={{float:"left"}} src={logo} alt="Learnify Logo" height={60} />
             </div>
-            <img src={notfound} style={{ width: "70%", height: "80.5%" }} alt="Learnify 404 Not Found" class = 'mockup'/> 
-            <button className="btn-blue" onClick={() => navigate(-1)}>Return</button>
+            <div className='notfoundpage-text'><h1>Oops! Something went wrong!</h1></div>
+            <img src={notfound} style={{ width: "60%"}} alt="Learnify 404 Not Found" class = 'mockup'/> 
+            <div>
+                <button className="btn-blue" onClick={() => navigate(-1)}>Return</button>
+            </div>
         </div>
     )       
 }
