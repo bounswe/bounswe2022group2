41ac9cc19bd6f9758eece4a6928c750b27facfe4
koraytekin <ge89zub@mytum.de>
2022-11-09 23:52:44 +0300

    finalization of Navigation Bar

--- learnify/web/src/pages/NavBar.js
+++ learnify/web/src/pages/NavBar.js
@@ -1,11 +1,11 @@
 import logo from '../images/logo-dblue.png' 
-import bell from '../images/NotificationBell.png'
+import bell from '../images/notification-icon (1).svg'
 export default function NavBar(){
     return <nav className="nav">
         <a href="/"><img src={logo} alt="Learnify Logo" height={60} /></a>
         <ul>
             
-                <label className="navSearch"><input type="text" placeholder="Search.."  size={40}/> </label>
+                <label className="navSearch"><input type="text" placeholder="Search.."  size={30}/> </label>
             
             <li>
                 <a href="/home" className='navBarText'><img src={bell} alt="Learnify Logo" height={30} /></a>
