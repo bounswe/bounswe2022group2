b31cd2d04bacd5dd68038e3fd89020a1e60df1fd
onurkmr <konur2727@gmail.com>
2022-11-18 18:17:58 +0300

    Removed unused imports.

--- learnify/mobile/lib/features/home-view-all/view/view_all_screen.dart
+++ learnify/mobile/lib/features/home-view-all/view/view_all_screen.dart
@@ -1,23 +1,16 @@
-import 'package:fl_chart/fl_chart.dart';
 import 'package:flutter/material.dart';
-import 'package:provider/provider.dart';
-import '../../../core/managers/navigation/navigation_manager.dart';
-import '../../../core/widgets/buttons/base_icon_button.dart';
-import '../../../product/constants/navigation_constants.dart';
-import '../../../product/language/language_keys.dart';
-import '../../home/view/home_screen.dart';
 
 import '../../../core/base/view/base_view.dart';
 import '../../../core/extensions/context/context_extensions.dart';
 import '../../../core/extensions/context/theme_extensions.dart';
-import '../../../core/helpers/selector_helper.dart';
-import '../../../core/managers/network/custom_interceptors.dart';
+import '../../../core/managers/navigation/navigation_manager.dart';
 import '../../../core/widgets/app-bar/default_app_bar.dart';
+import '../../../core/widgets/buttons/base_icon_button.dart';
 import '../../../core/widgets/text/base_text.dart';
-import '../../../product/constants/icon_keys.dart';
-import '../../home-wrapper/view-model/home_wrapper_view_model.dart';
+import '../../../product/constants/navigation_constants.dart';
 import '../../home/model/course_model.dart';
 import '../../home/view-model/home_view_model.dart';
+import '../../home/view/home_screen.dart';
 
 class ViewAllScreen extends BaseView<HomeViewModel> {
   ViewAllScreen(
