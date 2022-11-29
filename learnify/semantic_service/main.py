64d4eca5060e64fd0b7edb09e65805ca902c6f27
Batuhan Celik <batuhancelik.boun@gmail.com>
2022-11-29 15:40:26 +0300

    dockerization of semantic service

--- learnify/semantic_service/main.py
+++ learnify/semantic_service/main.py
@@ -12,18 +12,6 @@ from clip_client import Client
 config = dotenv_values(".env")
 app = FastAPI()
 
-
-def get_database():
-     
-   # Provide the mongodb atlas url to connect python to mongodb using pymongo
-   CONNECTION_STRING = "mongodb://localhost:27017"
- 
-   # Create a connection using MongoClient. You can import MongoClient or use pymongo.MongoClient
-   client = MongoClient(CONNECTION_STRING)
- 
-   # Create the database for our example (we will use the same database throughout the tutorial
-   return client['test']
-
 @app.on_event("startup")
 def startup_db_client():
     
