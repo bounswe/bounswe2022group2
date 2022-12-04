6f402c31f00bcdd8f77bfaf44d999aa402b1d951
ezgy <ezgibati@gmail.com>
2022-12-04 20:41:00 +0300

    implement random icon picker

--- learnify/mobile/lib/features/learning-space/models/create_ls_request_model.dart
+++ learnify/mobile/lib/features/learning-space/models/create_ls_request_model.dart
@@ -5,18 +5,20 @@ class CreateLSRequest extends BaseModel<CreateLSRequest> {
     this.title,
     this.description,
     this.categories,
+    this.iconId,
   });
 
   factory CreateLSRequest.fromJson(Map<String, dynamic> json) =>
       CreateLSRequest(
-        title: BaseModel.getByType<String>(json['title']),
-        description: BaseModel.getByType<String>(json['description']),
-        categories: BaseModel.getList(json['categories']),
-      );
+          title: BaseModel.getByType<String>(json['title']),
+          description: BaseModel.getByType<String>(json['description']),
+          categories: BaseModel.getList(json['categories']),
+          iconId: BaseModel.getByType<int>(json['icon_id']));
 
   final String? title;
   final String? description;
   final List<String>? categories;
+  final int? iconId;
 
   @override
   CreateLSRequest fromJson(Map<String, dynamic> json) =>
@@ -27,8 +29,9 @@ class CreateLSRequest extends BaseModel<CreateLSRequest> {
         'title': title,
         'description': description,
         'categories': categories,
+        'icon_id': iconId
       };
 
   @override
-  List<Object?> get props => <Object?>[title, description, categories];
+  List<Object?> get props => <Object?>[title, description, categories, iconId];
 }
