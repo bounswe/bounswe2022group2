19864050ce3f548e0a6038146ca3582120c202dc
ezgy <ezgibati@gmail.com>
2022-12-04 20:39:19 +0300

    add LS icon assets

--- learnify/mobile/lib/product/constants/icon_keys.dart
+++ learnify/mobile/lib/product/constants/icon_keys.dart
@@ -13,4 +13,27 @@ class IconKeys {
   static const String learnIllustration =
       'assets/images/learn-illustration.png';
   static const String userProfile = 'assets/images/user-profile.png';
+
+  static const List<String> lsIcons = <String>[
+    'assets/images/ls-icon-1.png',
+    'assets/images/ls-icon-2.png',
+    'assets/images/ls-icon-3.png',
+    'assets/images/ls-icon-4.png',
+    'assets/images/ls-icon-5.png',
+    'assets/images/ls-icon-6.png',
+    'assets/images/ls-icon-7.png',
+    'assets/images/ls-icon-8.png',
+    'assets/images/ls-icon-9.png',
+    'assets/images/ls-icon-10.png',
+    'assets/images/ls-icon-11.png',
+    'assets/images/ls-icon-12.png',
+    'assets/images/ls-icon-13.png',
+    'assets/images/ls-icon-14.png',
+    'assets/images/ls-icon-15.png',
+    'assets/images/ls-icon-16.png',
+    'assets/images/ls-icon-17.png',
+    'assets/images/ls-icon-18.png',
+    'assets/images/ls-icon-19.png',
+    'assets/images/ls-icon-20.png',
+  ];
 }
