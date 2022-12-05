41242c2a746364260e63b4f4cff615baba98e0aa
surmelienes1 <74325266+surmelienes1@users.noreply.github.com>
2022-12-05 22:26:09 +0300

    Update Post.js
    Updated the post component to enable post edit

--- learnify/web/src/components/Post.js
+++ learnify/web/src/components/Post.js
@@ -6,10 +6,242 @@ import edit from '../images/pen-to-square-regular.svg';
 import trash from '../images/trash-can-regular.svg';
 import author from '../images/create2.svg';
 import MDEditor from "@uiw/react-md-editor";
+import "@recogito/recogito-js/dist/recogito.min.css";
+import {useEffect, useRef } from "react";
+import { Recogito } from "@recogito/recogito-js";
+import { Annotorious } from "@recogito/annotorious";
+import "@recogito/annotorious/dist/annotorious.min.css";
+
+
+function TextInterface({
+    classes,
+    contentUrl,
+    handleNext,
+    handleCreateAnnotation
+  }) {
+    const contentRef = useRef();
+  
+    const [anno, setAnno] = useState(null);
+    const [text, setText] = useState(null);
+  
+    useEffect(() => {
+      // fetch(contentUrl).then((response) => {
+      //   response.text().then((t) => {
+      //     setText(t);
+      //   });
+      // });
+      setText(contentUrl);
+    }, [contentUrl]);
+  
+    useEffect(() => {
+      let annotorious = null;
+      // console.log("contentRef.current.parentNode", contentRef.current.parentNode);
+      if (classes && text && contentRef.current) {
+        anno && anno.destroy();
+        console.log("init anno", anno);
+  
+        const config = {
+          widgets: [
+            {
+              widget: "TAG",
+              vocabulary: classes
+            }
+          ],
+          readOnly: false,
+          content: contentRef.current
+        };
+  
+        annotorious = new Recogito(config);
+  
+        // Attach event handlers here
+        annotorious.on("createAnnotation", (annotation) => {
+          handleCreateAnnotation(annotation);
+        });
+      }
+      setAnno(annotorious);
+      return () => {
+        anno && anno.destroy();
+      };
+    }, [classes, text]);
+  
+    useEffect(() => {
+      console.log("anno changed");
+    }, [anno]);
+  
+    // ref={contentRef}
+    return (
+      <div>
+        <div ref={contentRef}>{text}</div>
+      </div>
+    );
+  }
 
 export default function Post(props){
 
+     // Ref to the image DOM element
+  const imgEl = useRef();
+
+  // The current Annotorious instance
+  const [anno, setAnno] = useState();
+  const [watch, setWatch] = useState(true);
+
+  // Current drawing tool name
+  const [tool, setTool] = useState("rect");
+
+  // Init Annotorious when the component
+  // mounts, and keep the current 'anno'
+  // instance in the application state
+  useEffect(() => {
+    let annotorious = null;
+
+    if (imgEl.current) {
+      // Init
+      annotorious = new Annotorious({
+        image: imgEl.current,
+        widgets: [
+          "COMMENT",
+          { widget: "TAG", vocabulary: ["Animal", "Building", "Waterbody"] }
+        ]
+      });
+
+      // Attach event handlers here
+      annotorious.on("createAnnotation", (annotation) => {
+        setWatch((prev) => !prev);
+        console.log("created", annotation, anno);
+      });
+
+      annotorious.on("updateAnnotation", (annotation, previous) => {
+        console.log("updated", annotation, previous);
+      });
+
+      annotorious.on("deleteAnnotation", (annotation) => {
+        console.log("deleted", annotation);
+      });
+
+      annotorious.on("createSelection", function () {
+        const defaultSuggestions =
+          "<p>EasterEgg</p><p>Baklava</p><p>Netting</p><p>Altay</p><p>Enes</p><p>Gökay</p><p>Koray</p>";
+        const commentWrapper = document.querySelector(".comment");
+        const commentTextarea = document.querySelector(".comment > textarea");
+        const commentSuggestion = document.createElement("div");
+        commentSuggestion.classList.add("comment-suggestion");
+        commentSuggestion.innerHTML = defaultSuggestions;
+        commentSuggestion.style.display = "none";
+        commentSuggestion.style.position = "absolute";
+        commentSuggestion.style.left = "3px";
+        commentSuggestion.style.backgroundColor = "#fff";
+        commentSuggestion.style.padding = "2px 12px";
+        commentSuggestion.style.boxShadow = "0 0 20px #00000040";
+        commentSuggestion.style.border = "1px solid #d6d7d9";
+        commentSuggestion.style.borderRadius = "3px";
+        commentSuggestion.style.zIndex = "1";
+        commentSuggestion.style.maxHeight = "100px";
+        commentSuggestion.style.overflowY = "auto";
+
+        if (commentTextarea) {
+          commentTextarea.addEventListener("keyup", () => {
+            let elements = [];
+            commentSuggestion.innerHTML = defaultSuggestions;
+            commentSuggestion.style.display = "block";
+
+            document
+              .querySelectorAll(".comment-suggestion > p")
+              .forEach((item) => {
+                if (
+                  item.innerText
+                    .toLowerCase()
+                    .includes(commentTextarea.value.toLowerCase())
+                ) {
+                  elements.push(`<p>${item.innerText}</p>`);
+                }
+              });
+
+            if (commentTextarea.value.toLowerCase() === "") {
+              commentSuggestion.innerHTML = defaultSuggestions;
+            } else if (elements.length < 1) {
+              commentSuggestion.style.display = "none";
+            } else {
+              commentSuggestion.innerHTML = elements.join("");
+            }
+          });
+        }
+
+        if (commentWrapper) {
+          commentWrapper.addEventListener("click", () => {
+            commentSuggestion.style.display = "block";
+          });
+          commentWrapper.style.position = "relative";
+          commentWrapper.appendChild(commentSuggestion);
+          document
+            .querySelectorAll(".comment-suggestion > p")
+            .forEach((item) => {
+              item.addEventListener("click", (e) => {
+                commentTextarea.value = e.target.innerText;
+                commentSuggestion.style.display = "none";
+              });
+            });
+        }
+
+        document.querySelectorAll(".comment-suggestion > p").forEach((item) => {
+          item.style.margin = "0 -12px";
+          item.style.padding = "2px 12px";
+
+          item.addEventListener("mouseover", () => {
+            item.style.backgroundColor = "rgb(189, 228, 255)";
+          });
+
+          item.addEventListener("mouseleave", () => {
+            item.style.backgroundColor = "#fff";
+          });
+        });
+      });
+    }
+
+    // Keep current Annotorious instance in state
+    setAnno(annotorious);
+    console.log("bganno", anno, annotorious?.getImageSnippetById());
+
+    // Cleanup: destroy current instance
+    return () => annotorious.destroy();
+  }, []);
+
+  useEffect(() => {
+    console.log("andso", anno?.getImageSnippetById());
+  }, [watch]);
+
+  // Toggles current tool + button label
+  const toggleTool = () => {
+    if (tool === "rect") {
+      setTool("polygon");
+      anno.setDrawingTool("polygon");
+    } else {
+      setTool("rect");
+      anno.setDrawingTool("rect");
+    }
+  };
+
+    const [i, setI] = useState(0);
+  const [url, setUrl] = useState();
+  const [classes, setClasses] = useState(["class1", "class2"]);
+
+  const urlList = [
+    ""
+  ];
+
+  useEffect(() => {
+    setUrl(urlList[i]);
+  }, [i]);
+
+  const handleNext = () => {
+    setI((i + 1) % urlList.length);
+  };
+
+  const handleCreateAnnotation = (annotation) => {
+    console.log("current URL", url);
+  };
+    
     const title = props.myPost.title;
+    const postId = props.myPost._id;
     const creator = props.myPost.creator;
     const content = props.myPost.content;
     const images = props.myPost.images;
@@ -17,6 +249,9 @@ export default function Post(props){
     const [upCounter, setUpCounter] = useState(0);
     const [downCounter, setDownCounter] = useState(0);
     const [deletePost, setDeletePost] = useState(false);
+
+    const [postTitle, setPostTitle] = useState("");
+    const [imageUrl, setImageUrl] = useState("");
 
     const increaseUp = () => {
         setUpCounter(count => count + 1);
@@ -28,6 +263,68 @@ export default function Post(props){
         setDeletePost(current => !current);
     };
 
+    const [editPost, setEditPost] = useState(false);
+
+    const editThePost = () => {
+        setValue(content);
+        setEditPost(current => !current);
+    };
+
+    const [value, setValue] = useState("");
+
+    const token = localStorage.getItem("token");
+
+    const [message, setMessage] = useState("");
+
+    const lsid = props.my_lsid;
+    
+    const handleSubmitEdit  = () => {
+        editExPost(lsid, postId, postTitle, value, imageUrl);
+    }
+    
+    const editExPost = async (lsid, postId, postTitle, final, imageUrl) => {
+        console.log(lsid)
+        console.log(postId)
+        console.log(postTitle)
+        console.log(final)
+        console.log(imageUrl)
+        await fetch(`${process.env.REACT_APP_BACKEND_BASE_URL}learningspace/edit/post`, {
+            method: "PUT",
+            body: JSON.stringify({
+                ls_id: lsid,
+                post_id: postId,
+                title: postTitle,
+                content: final,
+                images: [imageUrl],
+            }),
+            headers: {
+                'Content-type': 'application/json; charset=UTF-8',
+                'Authorization': `${token}` , 
+            },
+        })
+            .then((response) => {
+                if (response.status === 200) {
+                    console.log("successfull")
+                    
+                    response.json().then( json => {
+                        console.log(json.learningSpace.id)
+                    });
+                    console.log("Learning Space Post edited successfully!");
+                    window.location.reload();
+                } else {
+                    setMessage("Post could not be edited!");
+                    response.json().then( json => {
+                        console.log(json.resultMessage);
+                    });
+                }
+            }
+            )
+            .catch((error) => {
+                console.log(error);
+            }
+            );
+    };
+
     return(
     <div>
         {!deletePost &&
@@ -36,7 +333,12 @@ export default function Post(props){
                 {title}
             </label>
             <div className='space-5'></div>
+            <div>
+         <div>
+      </div>
+
                 <img
+                    ref={imgEl}
                     src={images}
                     alt="space"
                     style = {{width: "893.5px",
@@ -45,12 +347,16 @@ export default function Post(props){
                     objectFit: "cover",
                     objectPosition: "center",}}
                 />
+        </div>
             <div className='space-5'></div>
-                <MDEditor.Markdown
-                    style={{ padding: 15 }}
-                    source={content}
-                    linkTarget="_blank"
+            <div style={{ whiteSpace: "pre-wrap" }} className="">
+                <TextInterface
+                    classes={classes}
+                    contentUrl={content}
+                    handleNext={handleNext}
+                    handleCreateAnnotation={handleCreateAnnotation}
                 />
+            </div>
             <div className='space-5'></div>
             <div className='ls-buttons'>
                 <div className='ls-button-container2'>
@@ -72,20 +378,39 @@ export default function Post(props){
                     <label className="counter__output">{0}</label>
                 </div>
                 <div className='ls-button-container'>
-                    <button className="btn-orange" data-testid="forgotPassword">Annotations</button>
+                    <button className="btn-orange" onClick={toggleTool}> {tool === "rect" ? "RECTANGLE" : "POLYGON"}</button>              
                 </div>
                 <div className='ls-button-container-alt3'>
-                    <a href="/author" className='navBarText'><img src={author} alt="Learnify Logo" height={25} /></a>
+                <label className='navBarText'><img src={author} alt="Learnify Logo" height={25} /></label>
                 </div>
                     {creator}
                 <div className='ls-button-container-alt2'>
-                    <a href="/edit" className='navBarText'><img src={edit} alt="Learnify Logo" height={20} /></a>
+                <button className='ls-button'> <img src={edit} alt="Learnify Logo" height={20} onClick={editThePost}/> </button>
                 </div>
                 <div className='ls-button-container-alt2'>
                     <button className='ls-button'> <img src={trash} alt="Learnify Logo" height={20} onClick={deleteThePost}/> </button>
                 </div>
                 </div>
-                
+                {editPost && <div>
+                <div className='space-3'></div>
+                <div className='add-post-box-mid'>
+                            <label className="form__label" htmlFor="title">Post Title </label>
+                            <div className='space-3'></div>
+                            <span className='details-box2' role='textbox' value='nameMessage' contentEditable='true' onInput={(e) => setPostTitle(e.target.textContent)}></span>
+                            <div className='space-5'></div>
+                            <label className="form__label" htmlFor="imageLink">Image Link </label>
+                            <div className='space-3'></div>
+                            <span className='details-box2' role='textbox' value='nameMessage' contentEditable='true' onInput={(e) => setImageUrl(e.target.textContent)}></span>
+                            <div className='space-5'></div>
+                            <label className="form__label" htmlFor="postContent">Post Contents </label>
+                            <div className='space-3'></div>
+                    <MDEditor height={200} value={value} onChange={setValue} />
+                    <div className='space-8'></div>
+                    <div className='ls-button-container-alt4'>
+                    <button className="btn-orange" data-testid="forgotPassword" onClick={() => {handleSubmitEdit()}}>Submit</button>
+                    </div>
+                    </div>
+                </div>}
         </div>
         }
         </div>
