ccde8896018df67702ec3a11a73c27f55a4ffe17
bahricanyesil <bahrican.yesil@boun.edu.tr>
2022-12-17 12:10:16 +0300

    Remove separate center point and merge both center and market points

--- learnify/mobile/lib/features/learning-space/view/components/events/event_map.dart
+++ learnify/mobile/lib/features/learning-space/view/components/events/event_map.dart
@@ -11,12 +11,10 @@ class _EventMap extends StatefulWidget {
 class __EventMapState extends State<_EventMap> {
   @override
   Widget build(BuildContext context) {
-    final LatLng centerPoint =
-        LatLng(widget.location.latitude, widget.location.longitude);
     final LatLng markerPoint =
         LatLng(widget.location.latitude, widget.location.longitude);
     return FlutterMap(
-      options: MapOptions(center: centerPoint, maxZoom: 19),
+      options: MapOptions(center: markerPoint, maxZoom: 19),
       children: <Widget>[
         TileLayer(
           urlTemplate: 'https://tile.openstreetmap.org/{z}/{x}/{y}.png',
