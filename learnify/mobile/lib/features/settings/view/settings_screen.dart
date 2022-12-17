67763228d9beb2d6582e1c69bb35318b8910edc5
bahricanyesil <bahrican.yesil@boun.edu.tr>
2022-12-17 23:31:25 +0300

    Create base view and view model files

---
+++ learnify/mobile/lib/features/settings/view/settings_screen.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+import '../../../../core/base/view/base_view.dart';
+import '../../../core/extensions/context/context_extensions.dart';
+import '../view-model/settings_view_model.dart';
+
+class SettingsScreen extends BaseView<SettingsViewModel> {
+  const SettingsScreen({Key? key}) : super(builder: _builder, key: key);
+
+  static Widget _builder(BuildContext context) => Padding(
+        padding: EdgeInsets.symmetric(vertical: context.height * 3),
+        child: ListView.builder(itemBuilder: (BuildContext context, int i) {}),
+      );
+}
