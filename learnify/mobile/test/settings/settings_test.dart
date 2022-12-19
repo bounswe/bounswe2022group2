b56a6876c661862797ddb19c7041cafb9215e848
bahricanyesil <bahrican.yesil@boun.edu.tr>
2022-12-20 02:26:08 +0300

    Test settings item types

--- learnify/mobile/test/settings/settings_test.dart
+++ learnify/mobile/test/settings/settings_test.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
+import 'package:learnify/features/settings/constants/settings_options.dart';
 import 'package:learnify/features/settings/view/components/settings_item.dart';
 import 'package:learnify/features/settings/view/settings_screen.dart';
 
@@ -18,6 +19,15 @@ void main() {
 
       final List<Widget> settingsItems = tester.widgetList(itemFinder).toList();
       expect(settingsItems.length, equals(4));
+
+      final SettingsItem firstItem = settingsItems[0] as SettingsItem;
+      expect(firstItem.settings, equals(SettingsOptions.theme));
+      final SettingsItem item2 = settingsItems[1] as SettingsItem;
+      expect(item2.settings, equals(SettingsOptions.language));
+      final SettingsItem item3 = settingsItems[2] as SettingsItem;
+      expect(item3.settings, equals(SettingsOptions.appInfo));
+      final SettingsItem lastItem = settingsItems[3] as SettingsItem;
+      expect(lastItem.settings, equals(SettingsOptions.socialInfo));
     },
   );
 }
