d648ef26f3f09518b6aa74dac79d2284bcf5c0e7
xltvy <68481602+xltvy@users.noreply.github.com>
2022-12-19 16:25:18 +0300

    Icon svgs are imported

--- learnify/web/src/components/LearningSpacePrev.js
+++ learnify/web/src/components/LearningSpacePrev.js
@@ -1,5 +1,25 @@
 import * as React from 'react';
 import './component_styles.css';
+import lslogo_1 from '../images/ls_icons/ls-icon-1.svg'
+import lslogo_2 from '../images/ls_icons/ls-icon-2.svg'
+import lslogo_3 from '../images/ls_icons/ls-icon-3.svg'
+import lslogo_4 from '../images/ls_icons/ls-icon-4.svg'
+import lslogo_5 from '../images/ls_icons/ls-icon-5.svg'
+import lslogo_6 from '../images/ls_icons/ls-icon-6.svg'
+import lslogo_7 from '../images/ls_icons/ls-icon-7.svg'
+import lslogo_8 from '../images/ls_icons/ls-icon-8.svg'
+import lslogo_9 from '../images/ls_icons/ls-icon-9.svg'
+import lslogo_10 from '../images/ls_icons/ls-icon-10.svg'
+import lslogo_11 from '../images/ls_icons/ls-icon-11.svg'
+import lslogo_12 from '../images/ls_icons/ls-icon-12.svg'
+import lslogo_13 from '../images/ls_icons/ls-icon-13.svg'
+import lslogo_14 from '../images/ls_icons/ls-icon-14.svg'
+import lslogo_15 from '../images/ls_icons/ls-icon-15.svg'
+import lslogo_16 from '../images/ls_icons/ls-icon-16.svg'
+import lslogo_17 from '../images/ls_icons/ls-icon-17.svg'
+import lslogo_18 from '../images/ls_icons/ls-icon-18.svg'
+import lslogo_19 from '../images/ls_icons/ls-icon-19.svg'
+import lslogo_20 from '../images/ls_icons/ls-icon-20.svg'
 
 export default function LearningSpacePrev(props) {
     return (
