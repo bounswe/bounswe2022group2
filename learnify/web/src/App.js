6e76d53fb0a6413ddd0f87831c97d8c5cf807eb9
koraytekin <ge89zub@mytum.de>
2022-12-20 21:43:36 +0300

    added the navbar, footer;initialized the page

--- learnify/web/src/App.js
+++ learnify/web/src/App.js
@@ -17,6 +17,7 @@ import LearningSpace from './pages/LearningSpace';
 
 import CategoriesPage from './pages/CategoriesPage';
 import LSbyCategoryPage from './pages/LSbyCategoryPage';
+import ProfilePage from './pages/ProfilePage';
 
 
 function App() {
@@ -36,6 +37,7 @@ function App() {
           <Route path="/categories" element={<PrivateRoutes> <CategoriesPage /> </PrivateRoutes>}/>
           <Route path="/categories/:category" element={<PrivateRoutes> <LSbyCategoryPage /> </PrivateRoutes>}/>
           <Route path="/learningspace/:lsid" element={<PrivateRoutes> <LearningSpace /> </PrivateRoutes>}/>
+          <Route path="/profile" element={<PrivateRoutes> <ProfilePage /> </PrivateRoutes>}/>
           <Route path='*' element={<NotFoundPage/>} />
         </Routes>
       </BrowserRouter>
---
+++ learnify/web/src/pages/ProfilePage.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import './style.css'
+import Footer from '../components/Footer';
+import NavBar from '../components/NavBar';
+
+
+function ProfilePage() {
+    return(
+    <div className='profilepageLayout'>
+        <NavBar />
+        This is my Profile
+        
+              
+    
+        <Footer />
+    </div>
+
+            
+        )       
+    }
+    
+    export default ProfilePage;
+
