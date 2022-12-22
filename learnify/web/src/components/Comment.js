a2d70f2b10053c5b541823b54b7f8a6f001cc1fa
surmelienes1 <74325266+surmelienes1@users.noreply.github.com>
2022-12-22 16:28:28 +0300

    Create Comment.js
    Created the first version of the create comment component for our learning space structure working under each post.

---
+++ learnify/web/src/components/Comment.js
@@ -0,0 +1,193 @@
+import React, { useState } from 'react';
+import {useEffect, useRef } from "react";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { solid, regular } from '@fortawesome/fontawesome-svg-core/import.macro' // <-- import styles to be used
+import MDEditor from "@uiw/react-md-editor";
+
+export default function Comment(props){
+
+  const commentId = props.myComment._id;
+  const creator = props.myComment.creator;
+  const content = props.myComment.content;
+  const images = props.myComment.images;
+
+  const [upCounter, setUpCounter] = useState(0);
+  const [downCounter, setDownCounter] = useState(0);
+  const [deleteComment, setDeleteComment] = useState(false);
+
+  const [imageUrl, setImageUrl] = useState("");
+
+  const increaseUp = () => {
+      setUpCounter(count => count + 1);
+  };
+  const increaseDown = () => {
+      setDownCounter(count => count + 1);
+  };
+  const deleteTheComment = () => {
+      setDeleteComment(current => !current);
+  };
+
+  const [editComment, setEditComment] = useState(false);
+
+  const editTheComment = () => {
+      setValue(content);
+      setEditComment(current => !current);
+  };
+
+  const [value, setValue] = useState("");
+
+  const token = localStorage.getItem("token");
+
+  const [message, setMessage] = useState("");
+
+  const lsid = props.my_lsid;
+  
+  const handleSubmitEdit  = () => {
+     // editExComment(lsid, commentId, commentTitle, value, imageUrl);
+  }
+  /*
+  const editExComment = async (lsid, commentId, commentTitle, final, imageUrl) => {
+      console.log(lsid)
+      console.log(commentId)
+      console.log(commentTitle)
+      console.log(final)
+      console.log(imageUrl)
+      await fetch(`${process.env.REACT_APP_BACKEND_BASE_URL}learningspace/edit/comment`, {
+          method: "PUT",
+          body: JSON.stringify({
+              ls_id: lsid,
+              comment_id: commentId,
+              title: commentTitle,
+              content: final,
+              images: [imageUrl],
+          }),
+          headers: {
+              'Content-type': 'application/json; charset=UTF-8',
+              'Authorization': `${token}` , 
+          },
+      })
+          .then((response) => {
+              if (response.status === 200) {
+                  console.log("successfull")
+                  
+                  response.json().then( json => {
+                      console.log(json.learningSpace.id)
+                  });
+                  console.log("Learning Space Comment edited successfully!");
+                  window.location.reload();
+              } else {
+                  setMessage("Comment could not be edited!");
+                  response.json().then( json => {
+                      console.log(json.resultMessage);
+                  });
+              }
+          }
+          )
+          .catch((error) => {
+              console.log(error);
+          }
+          );
+  };
+  */
+
+    return(
+    <div>
+        {!deleteComment &&
+        <div className='ls-box-mid'>
+            <div className='space-5'></div>
+              {images[0] != "" && <div>
+                <img
+                    ref={imgEl}
+                    src={images}
+                    alt="space"
+                    style = {{width: "893.5px",
+                    height: "auto",
+                    maxWidth: "100%",
+                    objectFit: "cover",
+                    objectPosition: "center",}}
+                />
+            <div className='space-5'></div>
+            </div>
+        }
+            <MDEditor.Markdown
+                style={{ padding: 15 }}
+                source={content}
+                linkTarget="_blank"
+            />
+            <div className='space-5'></div>
+            <div className='post-buttons-contaier'>
+                <div className='post-box-left'>
+                    <div className='ls-button-container2'>
+                        <button className='post-upvote-button'>
+                            <FontAwesomeIcon icon={solid('caret-up')} color="green" onClick={increaseUp}/>
+                        </button>
+                    </div>
+                    <div className='post-container-display-item'>
+                        <label className="counter__output">{upCounter}</label>
+                    </div>
+                    <div className='ls-button-container2'>
+                        <button className='post-downvote-button'>
+                            <FontAwesomeIcon icon={solid('caret-down')} color="red" onClick={increaseDown}/>
+                        </button>
+                    </div>
+                    <div className='post-container-display-item'>
+                        <label className="counter__output">{downCounter}</label>
+                    </div>
+                    <div className='ls-button-container2'>
+                        <div className='post-comment-button'>
+                            <a href="/reply">
+                                <FontAwesomeIcon icon={regular('comment')} color="black"/>
+                            </a>
+                        </div>
+                    </div>
+                    <div className='post-container-display-item'>
+                        <label className="counter__output">{0}</label>
+                    </div>
+                </div>
+                <div className='annotation-selection-button'>
+                  {(images[0] != "") && 
+                    <button className="btn-orange" onClick={toggleTool}>{tool === "rect" ? "RECTANGLE" : "POLYGON"}</button>
+                   } 
+                </div>
+                <div className='post-box-right'>
+                    <div className='ls-button-container-alt3'>
+                        <label className='post-owner-icon-container'>
+                            <FontAwesomeIcon icon={solid('user-pen')} color="black"/>
+                        </label>
+                    </div>
+                    <div className='post-owner-display-item'>
+                        {creator}
+                    </div>
+                    <div className='ls-button-container-alt2'>
+                        <button className='post-edit-icon-container'>
+                                <FontAwesomeIcon icon={solid('edit')} color="black" onClick={editTheComment}/>
+                        </button>
+                    </div>
+                    <div className='ls-button-container-alt2'>
+                        <button className='post-delete-button'>
+                            <FontAwesomeIcon icon={solid('trash-can')} color="black"/>
+                        </button>
+                    </div>
+                </div>
+                </div>
+                {editComment && <div>
+                <div className='space-3'></div>
+                <div className='add-post-box-mid'>
+                            <label className="form__label" htmlFor="imageLink">Image Link </label>
+                            <div className='space-3'></div>
+                            <span className='details-box2' role='textbox' value='nameMessage' contentEditable='true' onInput={(e) => setImageUrl(e.target.textContent)}></span>
+                            <div className='space-5'></div>
+                            <label className="form__label" htmlFor="postContent">Comment Contents </label>
+                            <div className='space-3'></div>
+                    <MDEditor height={200} value={value} onChange={setValue} />
+                    <div className='space-8'></div>
+                    <div>
+                      <button className="btn-orange" data-testid="forgotPassword" onClick={() => {handleSubmitEdit()}}>Submit</button>
+                    </div>
+                    </div>
+                </div>}
+        </div>
+        }
+        </div>
+    );
+}
