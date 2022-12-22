2282ab95acc2d8efbbcc0e22736c991928811d88
xltvy <68481602+xltvy@users.noreply.github.com>
2022-12-22 21:41:57 +0300

    Initial layout of the page

---
+++ learnify/web/src/pages/MyLearningSpacesPage.js
@@ -0,0 +1,29 @@
+import React, { useState, useEffect } from 'react';
+
+import axios from 'axios';
+
+function MyLearningSpacesPage() {
+
+    return (
+        <div>
+            <NavBar/>
+            <div className="mylspage-container">
+                <div className="mylspage-wrapper">
+                    <div className="mylspage-title">
+                        <h1>Browsing the learning spaces of:</h1>
+                    </div>
+                    <div className="mylspage-header">
+                        <h1>{username}</h1>
+                    </div>
+                </div>
+                <div className="mylspage-body">
+                    {lsBoxes}
+                </div>
+            </div>
+            <Footer/>
+        </div>
+    )
+
+}
+
+export default MyLearningSpacesPage;
