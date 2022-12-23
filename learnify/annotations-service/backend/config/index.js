2e8452abd07755cfe43eeaad9630c3375f698083
Batuhan Celik <batuhancelik.boun@gmail.com>
2022-12-23 16:21:15 +0300

    perapare for mock deployment

--- learnify/annotations-service/backend/config/index.js
+++ learnify/annotations-service/backend/config/index.js
@@ -2,7 +2,7 @@ import { config } from 'dotenv';
 config();
 
 //NOTE: If you are running the project in an instance, you should store these secret keys in its configuration settings.
-const { PORT, NODE_ENV, DB_URI, JWT_KEY , DOCKER_DB_URI} = process.env
+const { PORT, NODE_ENV, DB_URI, JWT_KEY , DOCKER_DB_URI, LEARNIFY_FRONTEND_URL} = process.env
 
 export const port = PORT || 5000;
 export const env = NODE_ENV;
