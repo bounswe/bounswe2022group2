1ca46c32a7f713446cefc4a521ca96539173a2b4
xltvy <68481602+xltvy@users.noreply.github.com>
2022-12-23 13:08:36 +0300

    NavBar added

---
+++ learnify/web/src/pages/PopularLearningSpacesPage.js
@@ -0,0 +1,17 @@
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+import './style.css';
+import LearningSpaceDetailBox from '../components/LearningSpaceDetailBox';
+import Footer from '../components/Footer';
+import NavBar from '../components/NavBar';
+
+function PopularLearningSpacesPage() {
+
+    return (
+        <div>
+            <NavBar/>
+        </div>
+    )
+}
+
+export default PopularLearningSpacesPage;
