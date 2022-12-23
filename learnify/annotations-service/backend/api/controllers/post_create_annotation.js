2e85cf91062ec1a9e4c2006dad17c908826566ac
codingAku <egoldferb@gmail.com>
2022-12-23 13:33:06 +0300

    781 -  BUGFIX: Annotation ID numbers getting duplicated with deletion prevented

--- learnify/annotations-service/backend/api/controllers/post_create_annotation.js
+++ learnify/annotations-service/backend/api/controllers/post_create_annotation.js
@@ -20,8 +20,13 @@ export default async (req, res) => {
 
     let rgx = /[0-9]+$/;
     let annotations = await Annotation.find();
+    let count;
+    if(annotations.length != 0){
     let matches = (String(annotations.slice(-1)[0].id)).match(rgx);
-    let count = parseInt(matches[0]);
+    count = parseInt(matches[0]);
+    }else{
+        count = 0;
+    }
     let id = `http://frontURL/${req.params.ls_id}/${req.params.post_id}/anno${count + 1}`;
 
     let annotation = new Annotation({
