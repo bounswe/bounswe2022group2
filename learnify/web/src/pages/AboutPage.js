123ea774852aa63faf568579f03dc4b1f60a1ddf
surmelienes1 <74325266+surmelienes1@users.noreply.github.com>
2022-12-23 10:25:41 +0300

    Update AboutPage.js
    Updated the about page to include 2 markdown language rendering objects with each one having a goal to display information about developers separately

--- learnify/web/src/pages/AboutPage.js
+++ learnify/web/src/pages/AboutPage.js
@@ -1,13 +1,55 @@
 import React from 'react';
 import './style.css'
+import MDEditor from "@uiw/react-md-editor";
 import NavBar from '../components/NavBar';
 import Footer from '../components/Footer';
 
 function AboutPage() {
 
+    const [value, setValue] = React.useState("wikiPage");
+
+    const [valueSide, setValueSide] = React.useState("sideBar");
+
   return(  
     <div className='about-page-layout'>
         <NavBar />
+        <div className='learning-space'>
+        <div className='about-page-left'>
+        <MDEditor.Markdown
+            style={{ backgroundColor: "#f8f6f4",
+              width: "97%",
+              borderRadius: "20px",
+              marginTop: "5px",
+              marginBottom: "20px",
+              boxShadow: "0px 12px 16px -12px rgba(26,26,26,1)",
+              textAlign: "left",
+              marginRight: "auto",
+              marginLeft: "auto",
+              padding: "16px",
+              paddingLeft: "28px",
+              paddingRight: "28px"}}
+            source={value}
+            linkTarget="_blank"
+          />
+       </div>
+       <div className='about-page-right'>
+      <MDEditor.Markdown
+        style={{ backgroundColor: "#f8f6f4",
+              width: "97%",
+              borderRadius: "20px",
+              marginTop: "5px",
+              marginBottom: "20px",
+              marginRight: "auto",
+              boxShadow: "0px 12px 16px -12px rgba(26,26,26,1)",
+              textAlign: "left",
+              padding: "16px",
+              paddingLeft: "28px",
+              paddingRight: "28px"}}
+        source={valueSide}
+        linkTarget="_blank"
+      />
+      </div>
+      </div>
       <Footer />
       </div>
       )       
