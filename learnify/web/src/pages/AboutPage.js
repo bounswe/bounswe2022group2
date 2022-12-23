b56c19c15a40114bdd153e72482961df20e0a3f8
surmelienes1 <74325266+surmelienes1@users.noreply.github.com>
2022-12-23 10:24:01 +0300

    Create AboutPage.js
    Created the initial version of the about page where we will show the developers of the application

---
+++ learnify/web/src/pages/AboutPage.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import './style.css'
+import NavBar from '../components/NavBar';
+import Footer from '../components/Footer';
+
+function AboutPage() {
+
+  return(  
+    <div className='about-page-layout'>
+        <NavBar />
+      <Footer />
+      </div>
+      )       
+}
+
+export default AboutPage;
