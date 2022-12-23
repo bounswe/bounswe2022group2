5ea4237aa47bdd5a89ec8c755f68533da32fb486
xltvy <68481602+xltvy@users.noreply.github.com>
2022-12-23 13:10:33 +0300

    Page main content added

--- learnify/web/src/pages/PopularLearningSpacesPage.js
+++ learnify/web/src/pages/PopularLearningSpacesPage.js
@@ -10,6 +10,16 @@ function PopularLearningSpacesPage() {
     return (
         <div>
             <NavBar/>
+            <div className="popularlspage-container">
+                <div className="popularlspage-wrapper">
+                    <div className="popularlspage-title">
+                        <h1>Browsing the current most popular learning spaces:</h1>
+                    </div>
+                </div>
+                <div className="popularlspage-body" data-testid="popularls-body">
+                    {lsBoxes}
+                </div>
+            </div>
         </div>
     )
 }
