52bed74fd53370bbd0caeb8811170d021eb3293d
Batuhan Celik <batuhancelik.boun@gmail.com>
2022-12-24 11:01:03 +0300

    implement endpoint logic

---
+++ learnify/backend/api/controllers/events/post_participate_event.js
@@ -0,0 +1,43 @@
+import jwt from "jsonwebtoken";
+
+import { Event} from '../../../models/index.js';
+
+
+export default async (req, res) => {
+  var username;
+  console.log(req.headers);
+  try{
+    const authHeader = req.headers.authorization;
+    username = jwt.decode(authHeader).username;
+  }catch(e){
+    return res.status(401).json({ "resultMessage": "There is something wrong with your auth token."});
+  }
+
+  let event = await Event.findOne({_id: req.params.id})
+  .catch((err) => {
+    console.log("Could not fetch learning space from mongoDB")
+    console.log(err.message);
+    return res.status(500).json({ "resultMessage": "Something is wrong." });
+  });
+
+  if (!event) {
+    const err = "The event does not exists"
+    console.log(err)
+    return res.status(409).json({ "resultMessage": err });
+  }
+  if(!event.participants.includes(username)){
+      event.participants.push(username);
+      event.num_participants += 1 
+    }
+
+  event = await event.save().catch((err) =>{
+    console.log(err.message)
+    return res.status(500).json({ "resultMessage": "Could not save ls to DB" });
+  });
+
+
+  return res.status(200).json({
+    resultMessage: "Successfully participated.",
+    event: event
+  });
+};
