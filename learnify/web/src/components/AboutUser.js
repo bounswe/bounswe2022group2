6a941150aacea0ce01873032f84d11f246604fdf
gokayyildiz <yildizgokay2@gmail.com>
2022-12-24 15:23:38 +0300

    editable bio component

--- learnify/web/src/components/AboutUser.js
+++ learnify/web/src/components/AboutUser.js
@@ -2,54 +2,54 @@ import React, { useState } from 'react';
 import './component_styles.css';
 
 function AboutUser() {
-    const [aboutText, setAboutText] = useState(
+  const [aboutText, setAboutText] = useState(
     "This is a text instance about the user, the general information they want to give about themselves and their interests etc."
-    );
-    const [isEditing, setIsEditing] = useState(false);
+  );
+  const [isEditing, setIsEditing] = useState(false);
 
-    function handleEditButtonClick() {
+  function handleEditButtonClick() {
     setIsEditing(true);
-    }
+  }
 
-    function handleSaveButtonClick() {
+  function handleSaveButtonClick() {
     setIsEditing(false);
-    }
+  }
 
-    function EditButton({ onClick }) {
-        return (
-        <button className="edit-button" onClick={onClick}>
-        Edit
-        </button>
-        );
-        }
-
+  function EditButton({ onClick }) {
     return (
-    <div className="about-user-container">
-        {isEditing ? (
-            <>
-        <textarea
-        className="about-text-input"
-        value={aboutText}
-        onChange={(event) => setAboutText(event.target.value)}
-    />
-    <button className="save-button" onClick={handleSaveButtonClick}>
-        Save
-    </button>
-    </>
-    ) : (
-    <>
-    <label className="navBarText2">About this user</label>
-    <div className="space-3" />
-    <label className="about-text">{aboutText}</label>
-    <EditButton onClick={handleEditButtonClick} />
-    </>
-    )}
-    </div>
+      <button className="edit-button" onClick={onClick}>
+        Edit
+      </button>
     );
+  }
+
+  return (
+    <div className="about-user-container">
+      <div className="about-user-frame">
+        {isEditing ? (
+          <>
+            <textarea
+              className="about-text-input"
+              value={aboutText}
+              onChange={(event) => setAboutText(event.target.value)}
+            />
+            <div className="space-3" />
+            <button className="save-button" onClick={handleSaveButtonClick}>
+              Save
+            </button>
+          </>
+        ) : (
+          <>
+            <label className="navBarText2">Bio</label>
+            <div className="space-3" />
+            <label className="about-text">{aboutText}</label>
+            <div className="space-3" />
+            <EditButton onClick={handleEditButtonClick} />
+          </>
+        )}
+      </div>
+    </div>
+  );
 }
 
 export default AboutUser;
-
-
-
-
