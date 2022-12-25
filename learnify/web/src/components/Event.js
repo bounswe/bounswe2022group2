9a8afc7647344280b09912627d09356681c45f8c
surmelienes1 <74325266+surmelienes1@users.noreply.github.com>
2022-12-25 12:57:55 +0300

    Finalized the Event.js File
    Updated the event component of the learning space pages to finalize it in such a way that users can see the location of the event from a map and all the components look coherent and visually alluring.

--- learnify/web/src/components/Event.js
+++ learnify/web/src/components/Event.js
@@ -1,4 +1,7 @@
 import React, { useState, useEffect } from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { solid } from '@fortawesome/fontawesome-svg-core/import.macro' // <-- import styles to be used
+import Participants from './Participants';
 
 export default function Event(props){
 
@@ -72,7 +75,90 @@ export default function Event(props){
 
     return(
     <div>
-        
+        {!deleteEvent &&
+        <div className='ls-box-mid'>
+            <div className='event-box-right'>
+            <label className='feed-title' style={{fontSize: "30px"}}>
+                {eventTitle}
+            </label>
+                <FontAwesomeIcon icon={solid('circle-info')} onClick={showTheEvent} color="#4a07f5" style={{backgroundColor: "transparent",
+                    alignSelf: "center",
+                    marginLeft: "auto",
+                    border: "none",
+                    paddingTop: "6px",
+                    fontSize: "25px",
+                    cursor: "pointer",
+                    outline: "none"}}/>
+              </div>
+              <div className='space-5'></div>
+              <div className='event-box-right'>
+                {!isJoined ? <button className="btn-white2" disabled>Joined</button> : <button className="btn-orange" onClick={joinEvent}>Join</button>}
+                <FontAwesomeIcon icon={solid('people-group')} onClick={showTheParticipants} color="#000000" style={{backgroundColor: "transparent",
+                    alignSelf: "center",
+                    marginLeft: "auto",
+                    border: "none",
+                    paddingTop: "6px",
+                    fontSize: "25px",
+                    cursor: "pointer",
+                    outline: "none"}}/>
+               <div style={{marginLeft: "6.5px", fontSize: "20px", color: "#000000"}}>
+                    {num_participants}
+                    </div>
+              </div>
+                {!showParticipants && <div><div className='space-5'></div><div className='add-post-box-mid' style={{margin:"auto", marginBottom: "6.5px", marginTop: "6.5px"}}>
+                    {participantsArray.map(participant =>	
+                                    <Participants participant = {participant}/>)} </div></div>}
+            {!showEvent && <div>
+            <div className='space-5'></div>
+                {description}
+            <div className='space-5'></div>
+                <div className='event-buttons-contaier'>
+                        <div className='event-button-container-alt'>
+                            <label className='post-owner-icon-container'>
+                                <FontAwesomeIcon icon={solid('calendar-days')} color="black"/>
+                            </label>
+                        </div>
+                        <div className='event-owner-display-item'>
+                            {dformat}
+                        </div>
+                        <div className='event-button-container-alt'>
+                            <label className='post-owner-icon-container'>
+                                    <FontAwesomeIcon icon={solid('hourglass')} color="black"/>
+                            </label>
+                        </div>
+                        <div className='event-owner-display-item'>
+                            {duration}
+                        </div>
+                        </div>
+                        <div className='space-5'></div>
+                       
+                        <div>
+                            <iframe id="iframeId" height="230px" width="100%"></iframe>
+                        </div>
+            <div className='space-5'></div>
+            <div className='event-buttons-contaier'>
+                    <div className='event-button-container-alt'>
+                        <label className='post-owner-icon-container'>
+                            <FontAwesomeIcon icon={solid('user-pen')} color="black"/>
+                        </label>
+                    </div>
+                    <div className='post-owner-display-item'>
+                        {eventCreator}
+                    </div>
+                    <div className='event-button-container-alt'>
+                        <button className='post-edit-icon-container'>
+                            <FontAwesomeIcon icon={solid('edit')} color="black" onClick={editTheEvent}/>
+                        </button>
+                    </div>
+                    <div className='event-button-container-alt'>
+                        <button className='post-delete-button'>
+                            <FontAwesomeIcon icon={solid('trash-can')} color="black" onClick={deleteTheEvent}/>
+                        </button>
+                    </div>
+                </div>
+                </div>}
+        </div>
+        }
         </div>
     );
 }
