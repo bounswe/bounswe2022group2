196f826ddba7dbb12bc22ab1f5f72197a97fc50f
onurkmr <konur2727@gmail.com>
2022-12-25 03:40:10 +0300

    Added widget constants

---
+++ learnify/mobile/lib/features/others-profile/constants/other_widget_keys.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+
+mixin OtherProfileKeys {
+  static const Key usernameField = Key('username_field');
+  static const Key biographyField = Key('biography_field');
+  static const Key otherEnrolledLearningSpacesButton =
+      Key('other_enrolled_learning_spaces_button');
+}
--- learnify/mobile/lib/features/others-profile/view/others_profile_screen.dart
+++ learnify/mobile/lib/features/others-profile/view/others_profile_screen.dart
@@ -4,8 +4,11 @@ import '../../../core/base/view/base_view.dart';
 import '../../../core/extensions/context/context_extensions.dart';
 import '../../../core/extensions/context/theme_extensions.dart';
 import '../../../core/widgets/app-bar/default_app_bar.dart';
+import '../../../core/widgets/buttons/action_button.dart';
 import '../../../core/widgets/buttons/base_icon_button.dart';
 import '../../../core/widgets/text/base_text.dart';
+import '../../../product/language/language_keys.dart';
+import '../constants/other_widget_keys.dart';
 import '../view-model/others_profile_view_model.dart';
 
 class OthersProfileScreen extends BaseView<OthersProfileViewModel> {
@@ -35,4 +38,16 @@ class OthersProfileScreen extends BaseView<OthersProfileViewModel> {
             horizontal: context.responsiveSize * 3,
             vertical: context.responsiveSize * 2.5),
       );
+/*
+  static Widget get _enrolledLearningSpacesButton => ActionButton(
+        key: OtherProfileKeys.otherEnrolledLearningSpacesButton,
+        text: TextKeys.enrolledLS,
+        padding: EdgeInsets.symmetric(
+            horizontal: context.responsiveSize * 2.8,
+            vertical: context.responsiveSize * 1.4),
+        isActive: true, //learningSpaces.isNotEmpty,
+        onPressed: () async =>
+            context.read<HomeViewModel>().viewAll(TextKeys.takenLearningSpaces),
+      );
+      */
 }
