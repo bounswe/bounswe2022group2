92bf49a4ba086b2439d41b4c0d73847219f29748
surmelienes1 <74325266+surmelienes1@users.noreply.github.com>
2022-12-25 12:51:20 +0300

    Created the Event.js file
    Created the initial version of the event component with the given model of event from the backend side.

---
+++ learnify/web/src/components/Event.js
@@ -0,0 +1,23 @@
+import React from 'react';
+
+export default function Event(props){
+
+  const eventId = props.myEvent.id;
+  const eventCreator = props.myEvent.eventCreator;
+  const date = props.myEvent.date;
+  const description = props.myEvent.description;
+  const eventTitle = props.myEvent.title;
+  const duration = props.myEvent.duration + " minutes";
+  const participationLimit = props.myEvent.participationLimit;
+  const geolocation = props.myEvent.geolocation;
+  const latitude = geolocation.latitude;  
+  const longitude = geolocation.longitude;  
+  const participantsArray = props.myEvent.participants;
+  const num_participants = props.myEvent.num_participants;
+
+    return(
+    <div>
+    
+        </div>
+    );
+}
