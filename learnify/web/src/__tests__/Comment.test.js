164a464add47983c3e294cf4d3f34965efe1c74f
surmelienes1 <74325266+surmelienes1@users.noreply.github.com>
2022-12-26 20:46:59 +0300

    Create Comment.test.js
    Created the unit tests for the comment component of the posts of the learning space page in order to properly test the functionality behind.

---
+++ learnify/web/src/__tests__/Comment.test.js
@@ -0,0 +1,54 @@
+import React from "react";
+import Comment from "./../components/Comment";
+import { screen, render, fireEvent } from "@testing-library/react";
+import renderer from "react-test-renderer";
+
+const testComment = {
+  _id: "1",
+  creator: "user1",
+  content: "This is a test comment.",
+  images: [""],
+};
+
+describe("Comment", () => {
+  test("should render the comment correctly", async () => {
+    const component = renderer
+      .create(<Comment myComment={testComment} my_lsid="1" />)
+      .toJSON();
+    expect(component).toMatchSnapshot();
+  });
+
+  test("should render the upvote and downvote buttons", async () => {
+    render(<Comment myComment={testComment} my_lsid="1" />);
+    const upvoteButton = screen.getByTestId(/upvote-button/i);
+    const downvoteButton = screen.getByTestId(/downvote-button/i);
+    expect(upvoteButton).toBeInTheDocument();
+    expect(downvoteButton).toBeInTheDocument();
+  });
+
+  test("should increase the upvote and downvote counters when clicked", async () => {
+    render(<Comment myComment={testComment} my_lsid="1" />);
+    const upvoteButton = screen.getByTestId(/upvote-button/i);
+    const downvoteButton = screen.getByTestId(/downvote-button/i);
+    fireEvent.click(upvoteButton);
+    fireEvent.click(downvoteButton);
+    expect(upvoteButton.textContent).toBe("");
+    expect(downvoteButton.textContent).toBe("");
+  });
+
+  test("should render the delete button and delete the comment when clicked", async () => {
+    render(<Comment myComment={testComment} my_lsid="1" />);
+    const deleteButton = screen.getByTestId(/delete-button/i);
+    fireEvent.click(deleteButton);
+    expect(deleteButton).toBeInTheDocument();
+    expect(screen.queryByTestId("ls-box-mid")).toBeNull();
+  });
+
+  test("should render the edit button and the edit form when clicked", async () => {
+    render(<Comment myComment={testComment} my_lsid="1" />);
+    const editButton = screen.getByTestId(/edit-button/i);
+    fireEvent.click(editButton);
+    expect(editButton).toBeInTheDocument();
+});
+});
+
