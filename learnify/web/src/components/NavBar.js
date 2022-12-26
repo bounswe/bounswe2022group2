7d645b005aac199753c637e78b172422d91147cb
xltvy <68481602+xltvy@users.noreply.github.com>
2022-12-26 11:40:11 +0300

    User search component added

--- learnify/web/src/components/NavBar.js
+++ learnify/web/src/components/NavBar.js
@@ -9,15 +9,21 @@ export default function NavBar(){
     const [searchDecision, setSearchDecision] = useState(false);
 
     const [searchLsResults, setSearchLsResults] = useState([])
+    const [searchUserResults, setSearchUserResults] = useState([])
 
     const [searchLsInput, setSearchLsInput] = useState("");
+    const [searchUserInput, setSearchUserInput] = useState("");
     
-
     const currentUser = localStorage.getItem('username')
 
     const handleSearchLsChange = (e) => {
         e.preventDefault();
         setSearchLsInput(e.target.value);
+    }
+
+    const handleSearchUserChange = (e) => {
+        e.preventDefault();
+        setSearchUserInput(e.target.value);
     }
 
     useEffect(() => {
@@ -28,7 +34,16 @@ export default function NavBar(){
         getSearchLsResults()
     }, [searchLsInput])
 
+    useEffect(() => {
+        const getSearchUserResults = async () => {
+            const res = await axios.get(`${process.env.REACT_APP_BACKEND_BASE_URL}user/search/${searchUserInput}`)
+            setSearchUserResults(res.data.learning_spaces)
+        }
+        getSearchUserResults()
+    }, [searchUserInput])
+
     console.log(searchLsResults)
+    console.log(searchUserResults)
 
     return <nav className="nav" data-testid="navbar">
         <a href="/home"><img src={logo} alt="Learnify Logo Navbar" height={60} /></a>
@@ -39,6 +54,17 @@ export default function NavBar(){
 
                     <div className="search-results-container">
                         {searchLsResults.map(result => (
+                            <ul>
+                                <li className='search-result-item'><a href={`/learningspace/${result.id}`}>{result.title}</a></li>
+                            </ul>
+                        ))}
+                    </div>
+                </div>}
+                {searchDecision && <div className="user-search">
+                    <input className='search-input-field' type="text" placeholder="Search users"  size={30} onChange={handleSearchUserChange} value={searchUserInput}/>
+
+                    <div className="search-results-container">
+                        {searchUserResults.map(result => (
                             <ul>
                                 <li className='search-result-item'><a href={`/learningspace/${result.id}`}>{result.title}</a></li>
                             </ul>
