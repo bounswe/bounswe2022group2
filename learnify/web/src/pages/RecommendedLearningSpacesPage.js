173aa698278898a0db1c494fa0862a0f92dcbfc7
xltvy <68481602+xltvy@users.noreply.github.com>
2022-12-26 23:01:30 +0300

    Page layout added

--- learnify/web/src/pages/RecommendedLearningSpacesPage.js
+++ learnify/web/src/pages/RecommendedLearningSpacesPage.js
@@ -28,6 +28,22 @@ function PopularLearningSpacesPage() {
         <LearningSpaceDetailBox title={ls.title} description={ls.description} icon_id={ls.icon_id} num_participants={ls.num_participants} url={ls._id}/>
     ));
 
+    return (
+        <div>
+            <NavBar/>
+            <div className="recommendedlspage-container">
+                <div className="recommendedlspage-wrapper">
+                    <div className="recommendedlspage-title">
+                        <h1>Browsing the current most popular learning spaces:</h1>
+                    </div>
+                </div>
+                <div className="recommendedlspage-body" data-testid="recommendedlspage-body">
+                    {lsBoxes}
+                </div>
+            </div>
+            <Footer/>
+        </div>
+    )
 
 }
 
