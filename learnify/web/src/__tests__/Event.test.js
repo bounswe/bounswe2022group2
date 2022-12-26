93245b416745048faeb27fd60dc326f1e1cdeb2a
surmelienes1 <74325266+surmelienes1@users.noreply.github.com>
2022-12-26 22:04:10 +0300

    Create Event.test.js
    Created the first unit test for rendering the correct title for event component.

---
+++ learnify/web/src/__tests__/Event.test.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import { render, fireEvent } from '@testing-library/react';
+import Event from './../components/Event';
+
+describe('Event', () => {
+  it('renders the correct event title', () => {
+    const event = {
+      id: '1',
+      eventCreator: 'John',
+      date: '2022-01-01T12:00:00.000Z',
+      description: 'Event description',
+      title: 'Event Title',
+      duration: 60,
+      participationLimit: 10,
+      geolocation: {
+        latitude: 123,
+        longitude: 456
+      },
+      participants: [],
+      num_participants: 0
+    };
+    const { getByText } = render(<Event myEvent={event} />);
+    expect(getByText('Event Title')).toBeInTheDocument();
+  });
+});
