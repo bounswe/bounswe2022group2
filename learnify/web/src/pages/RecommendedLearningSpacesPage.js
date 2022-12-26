120bf2999a134dec4af01952876c9ccb9756b93b
xltvy <68481602+xltvy@users.noreply.github.com>
2022-12-26 22:59:29 +0300

    API call handler added

--- learnify/web/src/pages/RecommendedLearningSpacesPage.js
+++ learnify/web/src/pages/RecommendedLearningSpacesPage.js
@@ -11,6 +11,19 @@ function PopularLearningSpacesPage() {
 
     const token = localStorage.getItem('token');
 
+    useEffect(() => {
+        const getPopularLearningSpaces = async () => {
+            const res = await axios.get(`${process.env.REACT_APP_BACKEND_BASE_URL}learningspace/user/recomended`, {
+                headers: {
+                    'Content-type': 'application/json; charset=UTF-8',
+                    'Authorization': `${token}`,
+                }
+            });
+            setLearningspaces(res.data.learning_spaces);
+        }
+        getPopularLearningSpaces();
+    }
+
 }
 
 export default PopularLearningSpacesPage;
