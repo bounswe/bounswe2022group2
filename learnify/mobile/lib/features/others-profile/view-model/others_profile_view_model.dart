085575aaa02c395f6b81dd71979ff768a71fde39
onurkmr <konur2727@gmail.com>
2022-12-27 03:44:30 +0300

    Implemented other users profile page and backend connection.

--- learnify/mobile/lib/features/others-profile/view-model/others_profile_view_model.dart
+++ learnify/mobile/lib/features/others-profile/view-model/others_profile_view_model.dart
@@ -1,7 +1,26 @@
+import 'package:async/async.dart';
+
 import '../../../../core/base/view-model/base_view_model.dart';
+import '../../../core/managers/network/models/l_response_model.dart';
+import '../../learning-space/models/learning_space_model.dart';
+import '../../profile/model/get_profile_response_model.dart';
+import '../../profile/model/profile_model.dart';
+import '../../profile/service/i_profile_service.dart';
+import '../../profile/service/profile_service.dart';
 
 /// View model to manage the data on profile screen.
 class OthersProfileViewModel extends BaseViewModel {
+  OthersProfileViewModel();
+
+  late Profile _profile = const Profile(
+    username: "",
+  );
+  Profile get profile => _profile;
+
+  late final IProfileService _profileService = ProfileService.instance;
+
+  void setDefault() {}
+
   @override
   void initViewModel() {}
 
@@ -17,4 +36,32 @@ class OthersProfileViewModel extends BaseViewModel {
   }
 
   void _setDefault() {}
+
+  Future<String?> getProfile(String username) async {
+    await operation?.cancel();
+    operation =
+        CancelableOperation<String?>.fromFuture(_getProfileRequest(username));
+    final String? res = await operation?.valueOrCancellation();
+    return res;
+  }
+
+  Future<String?> _getProfileRequest(String username) async {
+    final IResponseModel<GetProfileResponse> res =
+        await _profileService.getProfileRequest(username);
+    final GetProfileResponse? respData = res.data;
+
+    if (res.hasError || respData == null) {
+      return res.error?.errorMessage;
+    }
+    _profile = Profile(
+      username: respData.username,
+      bio: respData.bio,
+      profilePicture: respData.profilePicture,
+      participated: respData.participated ?? <LearningSpace>[],
+      created: respData.created ?? <LearningSpace>[],
+    );
+    print(_profile.profilePicture);
+    notifyListeners();
+    return null;
+  }
 }
--- learnify/mobile/lib/features/others-profile/view/others_profile_screen.dart
+++ learnify/mobile/lib/features/others-profile/view/others_profile_screen.dart
@@ -1,27 +1,65 @@
-import 'package:flutter/material.dart';
+import 'dart:convert';
+import 'dart:io';
 
+import 'package:fl_chart/fl_chart.dart';
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:flutter/cupertino.dart';
 import '../../../core/base/view/base_view.dart';
 import '../../../core/extensions/context/context_extensions.dart';
 import '../../../core/extensions/context/theme_extensions.dart';
+import '../../../core/helpers/selector_helper.dart';
+import '../../../core/managers/navigation/navigation_manager.dart';
+import '../../../core/providers/theme/theme_provider.dart';
 import '../../../core/widgets/app-bar/default_app_bar.dart';
 import '../../../core/widgets/buttons/action_button.dart';
 import '../../../core/widgets/buttons/base_icon_button.dart';
+import '../../../core/widgets/divider/custom_vertical_divider.dart';
 import '../../../core/widgets/text/base_text.dart';
+import '../../../core/widgets/text/multiline_text.dart';
+import '../../../product/constants/icon_keys.dart';
+import '../../../product/constants/navigation_constants.dart';
 import '../../../product/language/language_keys.dart';
+import '../../../product/theme/dark_theme.dart';
+import '../../../product/theme/general_theme.dart';
+import '../../../product/theme/light_theme.dart';
+import '../../profile/model/profile_model.dart';
 import '../constants/other_widget_keys.dart';
 import '../view-model/others_profile_view_model.dart';
+
+part 'components/others_info.dart';
+part 'components/others_chart.dart';
 
 class OthersProfileScreen extends BaseView<OthersProfileViewModel> {
   OthersProfileScreen({required String username, Key? key})
       : super(
-          builder: (BuildContext context) => _builder(context, username),
+          builder: _builder,
           appBar: _appBarBuilder,
+          futureInit: (BuildContext context) async =>
+              context.read<OthersProfileViewModel>().getProfile(username),
           scrollable: true,
           key: key,
         );
 
-  static Widget _builder(BuildContext context, String username) =>
-      Center(child: Text(username));
+  static Widget _builder(BuildContext context) => Padding(
+        padding: EdgeInsets.symmetric(vertical: context.height * 3),
+        child: Column(
+          children: <Widget>[
+            _profilePhoto,
+            context.sizedH(1),
+            const _OthersInfo(),
+            context.sizedH(1),
+            _totalCountRow(context),
+            context.sizedH(2),
+            _enrolledLearningSpacesButton,
+            context.sizedH(1),
+            _createdLearningSpacesButton,
+            context.sizedH(1),
+            Transform.scale(scale: .85, child: const _ProfileChart()),
+            context.sizedH(1),
+          ],
+        ),
+      );
 
   static DefaultAppBar _appBarBuilder(BuildContext context) => DefaultAppBar(
         size: context.height * 6,
@@ -38,16 +76,119 @@ class OthersProfileScreen extends BaseView<OthersProfileViewModel> {
             horizontal: context.responsiveSize * 3,
             vertical: context.responsiveSize * 2.5),
       );
-/*
-  static Widget get _enrolledLearningSpacesButton => ActionButton(
-        key: OtherProfileKeys.otherEnrolledLearningSpacesButton,
-        text: TextKeys.enrolledLS,
-        padding: EdgeInsets.symmetric(
-            horizontal: context.responsiveSize * 2.8,
-            vertical: context.responsiveSize * 1.4),
-        isActive: true, //learningSpaces.isNotEmpty,
-        onPressed: () async =>
-            context.read<HomeViewModel>().viewAll(TextKeys.takenLearningSpaces),
+
+  static Widget get _profilePhoto =>
+      SelectorHelper<Profile?, OthersProfileViewModel>().builder(
+        (_, OthersProfileViewModel model) => model.profile,
+        (BuildContext context, Profile? profile, __) {
+          // ignore: unnecessary_cast
+          ImageProvider imageProvider = const AssetImage(IconKeys.userProfile);
+          late final CircleAvatar circleAvatar;
+          try {
+            if (profile!.profilePicture != null) {
+              final Image receivedImg =
+                  Image.memory(base64Decode(profile.profilePicture!));
+              imageProvider = receivedImg.image;
+            }
+            circleAvatar = CircleAvatar(
+              foregroundImage: imageProvider,
+              radius: context.width * 12,
+              foregroundColor: LightAppTheme.lightBlue,
+              backgroundColor: LightAppTheme.lightBlue,
+            );
+          } on FileSystemException catch (e) {
+            debugPrint(e.toString());
+            circleAvatar = const CircleAvatar();
+          }
+          return circleAvatar;
+        },
       );
-      */
+
+  static Widget get _enrolledLearningSpacesButton =>
+      SelectorHelper<Profile?, OthersProfileViewModel>().builder(
+        (_, OthersProfileViewModel model) => model.profile,
+        (BuildContext context, Profile? profile, _) => ActionButton(
+          text: TextKeys.enrolledLS,
+          padding: EdgeInsets.symmetric(
+              horizontal: context.responsiveSize * 2.8,
+              vertical: context.responsiveSize * 1.4),
+          isActive: profile!.participated.isNotEmpty,
+          onPressed: () async => NavigationManager.instance.navigateToPage(
+              path: NavigationConstants.viewall,
+              data: <String, dynamic>{
+                'listOfLearningSpaces': profile.participated,
+                'learningSpacesType': TextKeys.takenLearningSpaces,
+              }),
+        ),
+      );
+
+  static Widget get _createdLearningSpacesButton =>
+      SelectorHelper<Profile?, OthersProfileViewModel>().builder(
+        (_, OthersProfileViewModel model) => model.profile,
+        (BuildContext context, Profile? profile, _) => ActionButton(
+          text: TextKeys.createdLearningSpaces,
+          padding: EdgeInsets.symmetric(
+              horizontal: context.responsiveSize * 2.8,
+              vertical: context.responsiveSize * 1.4),
+          isActive: profile!.created.isNotEmpty,
+          onPressed: () async => NavigationManager.instance.navigateToPage(
+              path: NavigationConstants.viewall,
+              data: <String, dynamic>{
+                'listOfLearningSpaces': profile.created,
+                'learningSpacesType': TextKeys.createdLearningSpaces,
+              }),
+        ),
+      );
+
+  static Widget _totalCountRow(BuildContext context) =>
+      SelectorHelper<Profile, OthersProfileViewModel>().builder(
+        (_, OthersProfileViewModel model) => model.profile,
+        (BuildContext context, Profile profile, _) => Padding(
+          padding:
+              EdgeInsets.symmetric(horizontal: context.responsiveSize * 10),
+          child: ClipRRect(
+            borderRadius: BorderRadius.circular(15),
+            child: Container(
+              color: LightAppTheme.lightBlue.withOpacity(.8),
+              padding: EdgeInsets.all(context.responsiveSize * 2.5),
+              child: IntrinsicHeight(
+                child: Row(
+                  children: <Widget>[
+                    Expanded(child: _countColumn(context, 0, TextKeys.friends)),
+                    const CustomVerticalDivider(
+                        color: DarkAppTheme.lightActiveColor),
+                    Expanded(
+                        child: _countColumn(context,
+                            profile.participated.length, TextKeys.enrolled)),
+                    const CustomVerticalDivider(
+                        color: DarkAppTheme.lightActiveColor),
+                    Expanded(
+                        child: _countColumn(
+                            context, profile.created.length, TextKeys.created)),
+                  ],
+                ),
+              ),
+            ),
+          ),
+        ),
+      );
+  static Widget _countColumn(
+          BuildContext context, int countNumber, String titleKey) =>
+      Column(
+        children: <Widget>[
+          BaseText(
+            countNumber.toString(),
+            style: context.bodyLarge,
+            color: context.activeColor,
+            fontWeight: FontWeight.bold,
+            translated: false,
+          ),
+          context.sizedH(.3),
+          BaseText(
+            titleKey,
+            style: context.bodyMedium,
+            color: context.activeColor.withOpacity(.8),
+          ),
+        ],
+      );
 }
